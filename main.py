6b0d89bf572303e678b4f248efc13e085654bfe5
Izalpha <visualstudio530@naver.com>
2021-02-13 03:24:59 +0900

    calculrator done

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+def plus (a, b):
+    return int(a)+int(b);
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def minus (a, b):
+    return int(a)-int(b);
 
+def multiply (a, b):
+    return int(a)*int(b);
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def divide (a, b):
+    return int(a)/int(b);
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(plus(10, "12"));
