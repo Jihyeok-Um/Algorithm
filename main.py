cb2894941ae32b730e4d324742198c555b0de8fd
Izalpha <visualstudio530@naver.com>
2021-03-08 15:34:49 +0900

    4153

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-def short_time(x,y,w,h):
-    shortest = x
-    if (w-x < shortest):
-        shortest = w-x
-    if (y < shortest):
-        shortest = y
-    if (h-y < shortest):
-        shortest = h-y
-
-    print(shortest)
-
 def main():
-    num = list(map(int,input().split()))
-    short_time(num[0],num[1],num[2],num[3])
+    while(True):
+        num = list(map(int,input().split()))
+        if(num[0] == 0 and num[1] == 0 and num[2] == 0):
+            break
+        biggest = max(num[0], num[1], num[2])
+
+        if(num[0]*num[0] + num[1]*num[1] + num[2]*num[2] == biggest*biggest*2):
+            print("right")
+        else:
+            print("wrong")
+
 main()
