c38f29b9afb8113cb25a3f16d3351f424c699dca
Izalpha <visualstudio530@naver.com>
2021-03-15 08:21:02 +0900

    10819

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
-
 def getNextArray(array):
-    i = len(array)-1
-    while (i > 0 and array[i-1] >= array[i]):
-        i -= 1
-    if (i == 0):
-        return 0
+    max = 0
+    while(True):
+        i = len(array)-1
+        while (i > 0 and array[i-1] >= array[i]):
+            i -= 1
+        if (i == 0):
+            break
 
-    j = len(array)-1
-    while (array[j] <= array[i-1]):
-        j -= 1
-    array[j], array[i-1] = array[i-1], array[j]
+        j = len(array)-1
+        while (array[j] <= array[i-1]):
+            j -= 1
+        array[j], array[i-1] = array[i-1], array[j]
 
-    j = len(array) - 1
-    while (i <= j):
-        array[j], array[i] = array[i], array[j]
-        i += 1
-        j -= 1
+        j = len(array) - 1
+        while (i <= j):
+            array[j], array[i] = array[i], array[j]
+            i += 1
+            j -= 1
 
-    for i in range(len(array)):
-        print(array[i], end=' ')
+        sum = 0
+        for k in range(0, len(array)-1):
+            if (array[k]-array[k+1] < 0):
+                sum += (array[k] - array[k + 1]) * -1
+            else:
+                sum += (array[k] - array[k+1])
 
-    print()
-    return array
+        if (sum > max):
+            max = sum
+
+    return max
 
 def main():
     arraySize = int(input())
-    array = [i for i in range(1,arraySize+1)]
-    for i in range(len(array)):
-        print(array[i], end=' ')
-    print()
-    while(array != 0):
-        array = getNextArray(array)
+    array = list(map(int,input().split()))
+    array.sort()
+    print(getNextArray(array))
 
 main()
