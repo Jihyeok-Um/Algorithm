89d32ef7ddafb128c7df78cc96b70c982dc3d945
Izalpha <visualstudio530@naver.com>
2021-03-14 19:21:22 +0900

    10974

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+
+def getNextArray(array):
+    i = len(array)-1
+    while (i > 0 and array[i-1] >= array[i]):
+        i -= 1
+    if (i == 0):
+        return 0
+
+    j = len(array)-1
+    while (array[j] <= array[i-1]):
+        j -= 1
+    array[j], array[i-1] = array[i-1], array[j]
+
+    j = len(array) - 1
+    while (i <= j):
+        array[j], array[i] = array[i], array[j]
+        i += 1
+        j -= 1
+
+    for i in range(len(array)):
+        print(array[i], end=' ')
+
+    print()
+    return array
+
 def main():
-    num = list(map(int, input().split()))
-    nominator = num[0]
-    denominator = num[1]
-
-
-    for i in range(num[1]-1):
-        if denominator == 0:
-            break
-        num[0] = num[0]-1
-        num[1] = num[1]-1
-        nominator *= (num[0])
-        denominator *= (num[1])
-        print("{} {}".format(nominator,denominator))
-
-    if denominator == 0:
-        print(1)
-    else:
-        print("{}".format(int(nominator/denominator)))
+    arraySize = int(input())
+    array = [i for i in range(1,arraySize+1)]
+    for i in range(len(array)):
+        print(array[i], end=' ')
+    print()
+    while(array != 0):
+        array = getNextArray(array)
 
 main()
