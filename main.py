53e6ac0c524b28c8129d4c649f58c0c7213ee148
Izalpha <visualstudio530@naver.com>
2021-03-16 21:39:30 +0900

    10972

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
-def getNextArray(array):
-    max = 0
-    while(True):
-        i = len(array)-1
-        while (i > 0 and array[i-1] >= array[i]):
-            i -= 1
-        if (i == 0):
-            break
-
-        j = len(array)-1
-        while (array[j] <= array[i-1]):
-            j -= 1
-        array[j], array[i-1] = array[i-1], array[j]
-
-        j = len(array) - 1
-        while (i <= j):
-            array[j], array[i] = array[i], array[j]
-            i += 1
-            j -= 1
-
-        sum = 0
-        for k in range(0, len(array)-1):
-            if (array[k]-array[k+1] < 0):
-                sum += (array[k] - array[k + 1]) * -1
-            else:
-                sum += (array[k] - array[k+1])
-
-        if (sum > max):
-            max = sum
-
-    return max
-
 def main():
     arraySize = int(input())
-    array = list(map(int,input().split()))
-    array.sort()
-    print(getNextArray(array))
+    array = list(map(int, input().split()))
+
+    i = arraySize-1
+    while (i > 0 and array[i-1] > array[i]):
+        i -= 1
+
+    if (i == 0):
+        print(-1)
+        return 0
+
+    j = arraySize-1
+    while (array[j] < array[i-1]):
+        j -= 1
+    array[j], array[i-1] = array[i-1], array[j]
+
+    j = arraySize - 1
+    while (i <= j):
+        array[j], array[i] = array[i], array[j]
+        i += 1
+        j -= 1
+
+    for i in range(len(array)):
+        print(array[i], end=' ')
 
 main()
