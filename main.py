bc5e17237ea9ae652dbb6c837a6b9b8fc949700c
Izalpha <visualstudio530@naver.com>
2021-03-21 19:25:45 +0900

    14888 fail code

--- main.py
+++ main.py
@@ -1,34 +1,71 @@
 import sys
 sys.setrecursionlimit(200000)
-max = 0
+max = -2000000000
+min = 2000000000
 
-def getBiggestMoney(T, P, sum, day, i):
+def getResult(nArray, oArray, op1, op2, op3, op4, op, current, i):
+    if (i == 0):
+        if op == 0:
+            current = nArray[i] + nArray[i + 1]
+            op1 += 1
+        elif op == 1:
+            current = nArray[i] - nArray[i + 1]
+            op2 += 1
+        elif op == 2:
+            current = nArray[i] * nArray[i + 1]
+            op3 += 1
+        elif op == 3:
+            current = int(nArray[i] / nArray[i + 1])
+            op4 += 1
+            if (nArray[i] < 0 and nArray[i+1] > 0):
+                current = int(((-1*nArray[i]) / nArray[i + 1])*-1)
+    else:
+        if op == 0:
+            current = current + nArray[i + 1]
+            op1 += 1
+        elif op == 1:
+            current = current - nArray[i + 1]
+            op2 += 1
+        elif op == 2:
+            current = current * nArray[i + 1]
+            op3 += 1
+        elif op == 3:
+            current = int(current / nArray[i + 1])
+            op4 += 1
+            if (current < 0 and nArray[i+1] > 0):
+                current = int(((-1*current) / nArray[i + 1])*-1)
+
     global max
-    if (day == len(T)+1):
-        if (sum > max):
-            max = sum
+    global min
+
+    if (i == len(nArray)-2 and oArray[0] == op1 and oArray[1] == op2 and oArray[2] == op3 and oArray[3] == op4):
+        if current > max:
+            max = current
+        if current < min:
+            min = current
         return 0
-    elif (day > len(T)+1):
+    elif (i == len(nArray)-2):
         return 0
 
-    getBiggestMoney(T, P, sum+P[i], day+T[i], i+T[i])
-    getBiggestMoney(T, P, sum, day+1, i+1)
+    getResult(nArray, oArray, op1, op2, op3, op4, 0, current, i + 1)
+    getResult(nArray, oArray, op1, op2, op3, op4, 1, current, i + 1)
+    getResult(nArray, oArray, op1, op2, op3, op4, 2, current, i + 1)
+    getResult(nArray, oArray, op1, op2, op3, op4, 3, current, i + 1)
+
 
 def main():
     num = int(input())
-    T = []
-    P = []
-    sum = 0
-    day = 1
-    j = 0
+    nArray = list(map(int, input().split()))
+    oArray = list(map(int, input().split()))
+    i = 0
+    current = 0
+    op1 = op2 = op3 = op4 = 0
+    getResult(nArray, oArray, op1, op2, op3, op4, 0, current, i)
+    getResult(nArray, oArray, op1, op2, op3, op4, 1, current, i)
+    getResult(nArray, oArray, op1, op2, op3, op4, 2, current, i)
+    getResult(nArray, oArray, op1, op2, op3, op4, 3, current, i)
 
-    for i in range(num):
-        temp = list(map(int, input().split()))
-        T.append(temp[0])
-        P.append(temp[1])
-        temp = []
-
-    getBiggestMoney(T, P, sum, day, j)
     print(max)
+    print(min)
 
 main()
