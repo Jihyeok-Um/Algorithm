597c751d113538ef159d8145fdaf6c1c92905e41
Izalpha <visualstudio530@naver.com>
2021-03-21 17:28:49 +0900

    14501

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import sys
-pa = 0
+sys.setrecursionlimit(200000)
+max = 0
 
-def getSmallArray(howManyNum, array, currentSum, goalSum, count, add):
-    if (count == howManyNum):
-        if (currentSum == goalSum and add > 0):
-            global pa
-            pa += 1
+def getBiggestMoney(T, P, sum, day, i):
+    global max
+    if (day == len(T)+1):
+        if (sum > max):
+            max = sum
+        return 0
+    elif (day > len(T)+1):
         return 0
 
-    sys.setrecursionlimit(200000)
-    getSmallArray(howManyNum, array, currentSum+array[count], goalSum, count+1, add+1)
-    getSmallArray(howManyNum, array, currentSum, goalSum, count+1, add)
+    getBiggestMoney(T, P, sum+P[i], day+T[i], i+T[i])
+    getBiggestMoney(T, P, sum, day+1, i+1)
 
 def main():
-    temp = list(map(int, input().split()))
-    howManyNum = temp[0]
-    goalSum = temp[1]
-    array = list(map(int, input().split()))
-    currentSum = 0
-    count = 0
-    add = 0
-    getSmallArray(howManyNum, array, currentSum, goalSum, count, add)
-    print(pa)
+    num = int(input())
+    T = []
+    P = []
+    sum = 0
+    day = 1
+    j = 0
+
+    for i in range(num):
+        temp = list(map(int, input().split()))
+        T.append(temp[0])
+        P.append(temp[1])
+        temp = []
+
+    getBiggestMoney(T, P, sum, day, j)
+    print(max)
 
 main()
