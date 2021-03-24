7ee539a1d5925baf0b09f184ddbb96021203101b
Izalpha <visualstudio530@naver.com>
2021-03-25 00:27:06 +0900

    2231

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-import sys
-sys.setrecursionlimit(200000)
-max1 = -1000000000
-min1 = 1000000000
+def getMollaSum(num):
+    temp = num
+    sum = 0
+    j = 10
+    result = 0
+    for i in range(temp):
+        num -= i
+        temp2 = num
+        while (num % 1 != num):
+            sum += (num % 10)
+            num = int(num / 10)
+        if (temp == sum + temp2):
+            result = temp2
+        sum = 0
+        num = temp
 
-def getResult(nArray, op1, op2, op3, op4, current, i):
-    global max1, min1
-    if i == len(nArray):
-        max1 = max(current, max1)
-        min1 = min(current, min1)
-        return 0
-    else:
-        if op1:
-            getResult(nArray, op1-1, op2, op3, op4, current+nArray[i], i+1)
-        if op2:
-            getResult(nArray, op1, op2-1, op3, op4, current-nArray[i], i+1)
-        if op3:
-            getResult(nArray, op1, op2, op3-1, op4, current*nArray[i], i+1)
-        if op4:
-            getResult(nArray, op1, op2, op3, op4-1, int(current/nArray[i]), i+1)
+    print(result)
 
 def main():
     num = int(input())
-    nArray = list(map(int, input().split()))
-    op1, op2, op3, op4 = list(map(int, input().split()))
-
-    getResult(nArray, op1, op2, op3, op4, nArray[0], 1)
-    print(max1)
-    print(min1)
+    getMollaSum(num)
 
 main()
