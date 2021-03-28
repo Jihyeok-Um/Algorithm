b938875f4050bd0639a826dc2b8944155a0f30ea
Izalpha <visualstudio530@naver.com>
2021-03-29 01:59:55 +0900

    11729

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-def ddungci(hArray, wArray, nArray):
-    for i in range(0, len(hArray)):
-        for j in range(0, len(hArray)):
-            if (hArray[i] > hArray[j] and wArray[i] > wArray[j]):
-                nArray[j] += 1
-
-    for i in nArray:
-        print(i, end=' ')
-
-def main():
-    num = int(input())
-    nArray = []
-    hArray = []
-    wArray = []
-    for i in range(num):
-        nArray.append(1)
-    for i in range(num):
-        h,w = list(map(int, input().split()))
-        hArray.append(h)
-        wArray.append(w)
-
-    ddungci(hArray, wArray, nArray)
-
-main()
+n = int(input())
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        hanoi(n - 1, a, c, b)
+        print(a, c)
+        hanoi(n - 1, b, a, c)
+sum = 1
+for i in range(n - 1):
+    sum = sum * 2 + 1
+print(sum)
+hanoi(n, 1, 2, 3)
