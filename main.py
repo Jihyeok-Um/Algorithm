5bb0d4e8189b73b7697ba45c9ad3a2a71cf8f60e
Izalpha <visualstudio530@naver.com>
2021-03-29 23:30:20 +0900

    1260 인접 리스트 코드 고치기

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 node,edge,vertex = map(int, input().split())
-matrix = [[0]*node for i in range(node)]
+matrix = [ [] for i in range(node+1)]
 for i in range(edge):
     a,b = map(int, input().split())
-    matrix[a-1][b-1] = matrix[b-1][a-1] = 1
-visitList = [0] * (node)
+    matrix[a].append(b)
+    matrix[b].append(a)
+visitList = [0] * (node+1)
 
 def dfs(vertex):
     visitList[vertex]=1
-    print(vertex+1, end=' ')
-    for i in range(0, node):
-        if(visitList[i]==0 and matrix[vertex][i]==1):
-            dfs(i)
+    print(vertex, end=' ')
+    for i in range(1, node+1):
+        if(visitList[i]==0):
+            for j in range(0, len(matrix[vertex])):
+                if (matrix[vertex][j] == i):
+                    dfs(i)
 
 
 def bfs(vertex):
     queue=[vertex]
-    visitList[vertex]=0
+    visitList[vertex]=1
     while queue:
         vertex=queue.pop(0)
-        print(vertex+1, end=' ')
-        for i in range(0, node):
-            if(visitList[i]==1 and matrix[vertex][i]==1):
-                queue.append(i)
-                visitList[i]=0
+        print(vertex, end=' ')
+        for i in range(1, node+1):
+            if(visitList[i]==0):
+                for j in range(0, len(matrix[vertex])):
+                    if (matrix[vertex][j] == i):
+                        queue.append(i)
+                        visitList[i]=1
 
-dfs(vertex - 1)
+dfs(vertex)
+visitList = [0] * (node+1)
 print()
-bfs(vertex - 1)
+bfs(vertex)
