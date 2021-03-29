8d7fe042229aec78d0160190c4ce415d513353f0
Izalpha <visualstudio530@naver.com>
2021-03-29 19:34:00 +0900

    1260

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-n = int(input())
-def hanoi(n, a, b, c):
-    if n == 1:
-        print(a, c)
-    else:
-        hanoi(n - 1, a, c, b)
-        print(a, c)
-        hanoi(n - 1, b, a, c)
-sum = 1
-for i in range(n - 1):
-    sum = sum * 2 + 1
-print(sum)
-hanoi(n, 1, 2, 3)
+N,M,V=map(int,input().split())
+matrix=[[0]*(N) for i in range(N)]
+for i in range(M):
+    a,b = map(int,input().split())
+    matrix[a-1][b-1]=matrix[b-1][a-1]=1
+visit_list=[0]*(N)
+
+def dfs(V):
+    visit_list[V]=1
+    print(V+1, end=' ')
+    for i in range(0,N):
+        if(visit_list[i]==0 and matrix[V][i]==1):
+            dfs(i)
+
+def bfs(V):
+    queue=[V]
+    visit_list[V]=0
+    while queue:
+        V=queue.pop(0)
+        print(V+1, end=' ')
+        for i in range(0, N):
+            if(visit_list[i]==1 and matrix[V][i]==1):
+                queue.append(i)
+                visit_list[i]=0
+
+dfs(V-1)
+print()
+bfs(V-1)
