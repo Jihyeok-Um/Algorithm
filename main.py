8c045419eeb9f925503696e0c2fe6de935e7e353
Izalpha <visualstudio530@naver.com>
2021-03-29 20:18:59 +0900

    1260 my code

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-N,M,V=map(int,input().split())
-matrix=[[0]*(N) for i in range(N)]
-for i in range(M):
-    a,b = map(int,input().split())
-    matrix[a-1][b-1]=matrix[b-1][a-1]=1
-visit_list=[0]*(N)
+node,edge,vertex = map(int, input().split())
+matrix = [[0]*node for i in range(node)]
+for i in range(edge):
+    a,b = map(int, input().split())
+    matrix[a-1][b-1] = matrix[b-1][a-1] = 1
+visitList = [0] * (node)
 
-def dfs(V):
-    visit_list[V]=1
-    print(V+1, end=' ')
-    for i in range(0,N):
-        if(visit_list[i]==0 and matrix[V][i]==1):
+def dfs(vertex):
+    visitList[vertex]=1
+    print(vertex+1, end=' ')
+    for i in range(0, node):
+        if(visitList[i]==0 and matrix[vertex][i]==1):
             dfs(i)
 
-def bfs(V):
-    queue=[V]
-    visit_list[V]=0
-    while queue:
-        V=queue.pop(0)
-        print(V+1, end=' ')
-        for i in range(0, N):
-            if(visit_list[i]==1 and matrix[V][i]==1):
-                queue.append(i)
-                visit_list[i]=0
 
-dfs(V-1)
+def bfs(vertex):
+    queue=[vertex]
+    visitList[vertex]=0
+    while queue:
+        vertex=queue.pop(0)
+        print(vertex+1, end=' ')
+        for i in range(0, node):
+            if(visitList[i]==1 and matrix[vertex][i]==1):
+                queue.append(i)
+                visitList[i]=0
+
+dfs(vertex - 1)
 print()
-bfs(V-1)
+bfs(vertex - 1)
