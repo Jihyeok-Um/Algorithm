31bc36d56ba038042a3dd2a906f4dacdcbc64ac3
Izalpha <visualstudio530@naver.com>
2021-04-04 05:31:16 +0900

    2667

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 node = int(input())
-edge = int(input())
+matrix = []
+array = []
+countList = []
 count = 0
-matrix = [[0]*(node+1) for i in range(node+1)]
-for i in range(edge):
-    a,b = map(int, input().split())
-    matrix[a][b] = 1
-    matrix[b][a] = 1
-visitList = [0 for i in range(node+1)]
+for i in range(node):
+     array = input()
+     matrix.append(array)
+visitList = [ [0]*(node) for i in range(node)]
 
-def dfs(vertex):
+def dfs(x,y):
     global count
-    visitList[vertex] = 1
-    for i in range(1, node+1):
-        if (visitList[i] == 0 and matrix[vertex][i] == 1):
-            count += 1
-            dfs(i)
+    count += 1
+    visitList[x][y] = 1
+    if (x-1 >= 0 and visitList[x-1][y] == 0 and matrix[x-1][y] == '1'):
+        dfs(x-1,y)
+    if (x+1 < node and visitList[x+1][y] == 0 and matrix[x+1][y] == '1'):
+        dfs(x+1,y)
+    if (y-1 >= 0 and visitList[x][y-1] == 0 and matrix[x][y-1] == '1'):
+        dfs(x,y-1)
+    if (y+1 < node and visitList[x][y+1] == 0 and matrix[x][y+1] == '1'):
+        dfs(x,y+1)
 
-dfs(1)
-print(count)
+for i in range(0,node):
+    for j in range(0,node):
+        if (matrix[i][j] != '0' and visitList[i][j] != 1):
+            dfs(i,j)
+        if (count != 0):
+            countList.append(count)
+        count = 0
+
+print(len(countList))
+countList.sort()
+for i in countList:
+    print(i)
