ffdfb66fdbe87abe9d8721f40c38367f9fe5bdde
Izalpha <visualstudio530@naver.com>
2021-04-04 06:14:48 +0900

    1012

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-node = int(input())
-matrix = []
-array = []
-countList = []
-count = 0
-for i in range(node):
-     array = input()
-     matrix.append(array)
-visitList = [ [0]*(node) for i in range(node)]
+import sys
+sys.setrecursionlimit(10**6)
 
-def dfs(x,y):
-    global count
-    count += 1
+def dfs(x, y):
+    global a, b
     visitList[x][y] = 1
-    if (x-1 >= 0 and visitList[x-1][y] == 0 and matrix[x-1][y] == '1'):
-        dfs(x-1,y)
-    if (x+1 < node and visitList[x+1][y] == 0 and matrix[x+1][y] == '1'):
-        dfs(x+1,y)
-    if (y-1 >= 0 and visitList[x][y-1] == 0 and matrix[x][y-1] == '1'):
-        dfs(x,y-1)
-    if (y+1 < node and visitList[x][y+1] == 0 and matrix[x][y+1] == '1'):
-        dfs(x,y+1)
+    if (x - 1 >= 0 and visitList[x - 1][y] == 0 and matrix[x - 1][y] == 1):
+        dfs(x - 1, y)
+    if (x + 1 < a and visitList[x + 1][y] == 0 and matrix[x + 1][y] == 1):
+        dfs(x + 1, y)
+    if (y - 1 >= 0 and visitList[x][y - 1] == 0 and matrix[x][y - 1] == 1):
+        dfs(x, y - 1)
+    if (y + 1 < b and visitList[x][y + 1] == 0 and matrix[x][y + 1] == 1):
+        dfs(x, y + 1)
 
-for i in range(0,node):
-    for j in range(0,node):
-        if (matrix[i][j] != '0' and visitList[i][j] != 1):
-            dfs(i,j)
-        if (count != 0):
-            countList.append(count)
-        count = 0
+num = int(input())
+for i in range(num):
+    a,b,node = map(int,input().split())
+    matrix = [[0]*(b) for i in range(a)]
+    visitList = [ [0]*(b) for i in range(a)]
+    count = 0
 
-print(len(countList))
-countList.sort()
-for i in countList:
-    print(i)
+    for i in range(node):
+        x,y = map(int,input().split())
+        matrix[x][y] = 1
+
+    for i in range(a):
+        for j in range(b):
+            if (matrix[i][j] == 1 and visitList[i][j] == 0):
+                dfs(i,j)
+                count += 1
+
+    print(count)
