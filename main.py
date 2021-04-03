87a795a42d772ca058cca94d4d3fba6e61e4d69c
Izalpha <visualstudio530@naver.com>
2021-04-04 01:16:02 +0900

    2606

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-num = int(input())
-array = []
-for i in range(num):
-    array.append(int(input()))
-array.sort()
-for i in range(0,num):
-    print(array[i])
+node = int(input())
+edge = int(input())
+count = 0
+matrix = [[0]*(node+1) for i in range(node+1)]
+for i in range(edge):
+    a,b = map(int, input().split())
+    matrix[a][b] = 1
+    matrix[b][a] = 1
+visitList = [0 for i in range(node+1)]
+
+def dfs(vertex):
+    global count
+    visitList[vertex] = 1
+    for i in range(1, node+1):
+        if (visitList[i] == 0 and matrix[vertex][i] == 1):
+            count += 1
+            dfs(i)
+
+dfs(1)
+print(count)
