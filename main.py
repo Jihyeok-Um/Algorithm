bbdc96fcb60eaa24221209e4072790448080ffaf
Izalpha <visualstudio530@naver.com>
2021-04-10 11:31:28 +0900

    4375

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-def dfs(x, y):
-    global a, b
-    visitList[x][y] = 1
-    if (x - 1 >= 0 and visitList[x - 1][y] == 0 and matrix[x - 1][y] == 1):
-        dfs(x - 1, y)
-    if (x + 1 < a and visitList[x + 1][y] == 0 and matrix[x + 1][y] == 1):
-        dfs(x + 1, y)
-    if (y - 1 >= 0 and visitList[x][y - 1] == 0 and matrix[x][y - 1] == 1):
-        dfs(x, y - 1)
-    if (y + 1 < b and visitList[x][y + 1] == 0 and matrix[x][y + 1] == 1):
-        dfs(x, y + 1)
-
-num = int(input())
-for i in range(num):
-    a,b,node = map(int,input().split())
-    matrix = [[0]*(b) for i in range(a)]
-    visitList = [ [0]*(b) for i in range(a)]
-    count = 0
-
-    for i in range(node):
-        x,y = map(int,input().split())
-        matrix[x][y] = 1
-
-    for i in range(a):
-        for j in range(b):
-            if (matrix[i][j] == 1 and visitList[i][j] == 0):
-                dfs(i,j)
-                count += 1
-
+while(True):
+    try:
+        num = int(input())
+    except EOFError:
+        break
+    temp = 1
+    count = 1
+    while(temp % num != 0):
+       temp = (temp * 10) + 1
+       count += 1
     print(count)
