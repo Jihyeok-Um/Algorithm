facbf01da540eb6b92d4a9bad225c979b1c3e993
Izalpha <visualstudio530@naver.com>
2021-04-11 20:39:07 +0900

    17427

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-while(True):
-    try:
-        num = int(input())
-    except EOFError:
-        break
-    temp = 1
-    count = 1
-    while(temp % num != 0):
-       temp = (temp * 10) + 1
-       count += 1
-    print(count)
+num = int(input())
+result = 0
+for i in range(1, num+1):
+     result += (int(num/i))*i
+
+print(result)
