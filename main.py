b12ef8677b20db22287fd9e66bebbc60b9d3d4af
Izalpha <visualstudio530@naver.com>
2021-04-14 17:54:45 +0900

    3085

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
-num = int(input())
-result = 0
-for i in range(1, num+1):
-     result += (int(num/i))*i
+max = 1
 
-print(result)
+def swapCandy(array):
+    global num
+    for i in range(0, num):
+        for j in range(0, num-1):
+            array[i][j], array[i][j+1] = array[i][j+1], array[i][j]
+            checkCandy(array)
+            array[i][j], array[i][j+1] = array[i][j+1], array[i][j]
+
+    for i in range(0, num):
+        for j in range(0, num-1):
+            array[j][i], array[j+1][i] = array[j+1][i], array[j][i]
+            checkCandy(array)
+            array[j][i], array[j+1][i] = array[j+1][i], array[j][i]
+
+def checkCandy(array):
+    global max
+    current = 1
+    for i in range(0, num):
+        current = 1
+        for j in range(0, num-1):
+            if (array[i][j] == array[i][j+1]):
+                current += 1
+                if (current > max):
+                    max = current
+            else:
+                current = 1
+
+    for i in range(0, num):
+        current = 1
+        for j in range(0, num-1):
+            if (array[j][i] == array[j+1][i]):
+                current += 1
+                if (current > max):
+                    max = current
+            else:
+                current = 1
+
+
+num = int(input())
+outerArray = []
+for i in range(num):
+    outerArray.append(list(input()))
+
+swapCandy(outerArray)
+print(max)
