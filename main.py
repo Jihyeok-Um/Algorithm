45ae2b4cd3706d67893241f27a0c1d33228233dc
Izalpha <visualstudio530@naver.com>
2021-04-18 04:08:15 +0900

    14500

--- main.py
+++ main.py
@@ -1,46 +1,71 @@
-num = int(input())
-start = 100
-min_ = 10000000
-channelList = []
-brokenNum = int(input())
-if (brokenNum != 0):
-    broken = list(map(int, input().split()))
-else:
-    broken = []
-count_ = 0
-value_ = 0
+matrix = []
+max_ = 0
+sero,garo = map(int,input().split())
+for i in range(sero):
+    matrix.append(list(map(int,input().split())))
 
-def makeChannel(broken):
-    global channelList
-    for i in range(1000001):
-        if (isBroken(i,broken)):
-            channelList.append(i)
+def getScore(sero, garo, matrix):
+    temp = 0
+    global max_
+    for i in range(0, sero):
+        for j in range(0, garo):
+            if (sero-1 >= i and garo-1 >= j+3):#1
+                temp = matrix[i][j]+matrix[i][j+1]+matrix[i][j+2]+matrix[i][j+3]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+3 and garo-1 >= j):#2
+                temp = matrix[i][j]+matrix[i+1][j]+matrix[i+2][j]+matrix[i+3][j]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+1):#3
+                temp = matrix[i][j]+matrix[i+1][j]+matrix[i][j+1]+matrix[i+1][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#4
+                temp = matrix[i][j]+matrix[i+1][j]+matrix[i][j+1]+matrix[i][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#5
+                temp = matrix[i][j]+matrix[i+1][j]+matrix[i+1][j+1]+matrix[i+1][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#6
+                temp = matrix[i+1][j]+matrix[i+1][j+1]+matrix[i+1][j+2]+matrix[i][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#7
+                temp = matrix[i][j]+matrix[i][j+1]+matrix[i][j+2]+matrix[i+1][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#15
+                temp = matrix[i][j]+matrix[i+1][j]+matrix[i+1][j+1]+matrix[i+2][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#16
+                temp = matrix[i+1][j]+matrix[i+1][j+1]+matrix[i+2][j]+matrix[i][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#17
+                temp = matrix[i+1][j]+matrix[i+1][j+1]+matrix[i][j+1]+matrix[i][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#8
+                temp = matrix[i][j] + matrix[i][j+1] + matrix[i+1][j+1] + matrix[i+1][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#9
+                temp = matrix[i][j] + matrix[i+1][j] + matrix[i+2][j] + matrix[i+2][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#18
+                temp = matrix[i+2][j] + matrix[i+2][j+1] + matrix[i+1][j+1] + matrix[i][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#10
+                temp = matrix[i][j] + matrix[i+1][j] + matrix[i+2][j] + matrix[i][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#19
+                temp = matrix[i][j] + matrix[i][j+1] + matrix[i+1][j+1] + matrix[i+2][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#11
+                temp = matrix[i][j] + matrix[i][j+1] + matrix[i+1][j+1] + matrix[i][j+2]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+1 and garo-1 >= j+2):#12
+                temp = matrix[i+1][j] + matrix[i+1][j+1] + matrix[i+1][j+2] + matrix[i][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#13
+                temp = matrix[i+1][j] + matrix[i][j+1] + matrix[i+1][j+1] + matrix[i+2][j+1]
+                max_ = max(temp, max_)
+            if (sero-1 >= i+2 and garo-1 >= j+1):#14
+                temp = matrix[i][j] + matrix[i+1][j+1] + matrix[i+1][j] + matrix[i+2][j]
+                max_ = max(temp, max_)
 
-def isBroken(channel, broken):
-    if (channel == 0):
-        if (broken.count(0) != 0):
-            return False
-
-    while(channel != 0):
-        if (broken.count(int(channel % 10)) != 0):
-            return False
-        channel = int(channel / 10)
-    return True
-
-temp = abs(num - start)
-makeChannel(broken)
-for i in range(0, len(channelList)):
-    count_ = 0
-    if (channelList[i] == 0):
-        count_ = 1
-    value_ = abs(num - channelList[i])
-    while(channelList[i] != 0):
-        channelList[i] = int(channelList[i] / 10)
-        count_ += 1
-
-    min_ = min(min_, min(count_+value_, temp))
-if (len(channelList) == 0):
-    print(temp)
-else:
-    print(min_)
-
+getScore(sero, garo, matrix)
+print(max_)
