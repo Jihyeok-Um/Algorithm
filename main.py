3fde3e0c8a59a770397cf571a0adcaca0a18e960
Izalpha <visualstudio530@naver.com>
2021-04-20 07:32:32 +0900

    15652

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ a = [0]*m
 
 def go(index, n, m):
     if index == m:
+        for i in range(1, index):
+            if (a[i] < a[i-1]):
+                return
         sys.stdout.write(' '.join(map(str,a))+'\n')
         return
     for i in range(1, n+1):
