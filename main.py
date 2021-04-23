6c5d5246f230cd5290821c61c691c43ce9d07547
Izalpha <visualstudio530@naver.com>
2021-04-23 14:50:40 +0900

    14889

--- main.py
+++ main.py
@@ -3,34 +3,31 @@ limit_number = 15000
 sys.setrecursionlimit(limit_number)
 line = int(input())
 matrix = []
-check = [[False for j in range(line)]for i in range(line)]
-teamA = teamB = 0
 ans = 100000
 for i in range(line):
     matrix.append(list(map(int, input().split())))
 
-def getScore(index, addA, teamA, teamB):
-    global ans
-    if (addA > line/2):
-        return
 
-    if (index == line):
-        if(addA == line/2):
-            ans = min(abs(teamA - teamB),ans)
-            print(ans)
+def getScore(index, teamA, teamB):
+    global ans
+    aScore = bScore = 0
+    if(index == line):
+        if(line / 2 != len(teamA)):
+            return
+        if(line / 2 != len(teamB)):
             return
 
-    for y in range(0,line):
-        for x in range(0,line):
-            if (x == y):
-                continue
-            if (check[y][x]):
-                continue
+        for y in range(0, len(teamA)):
+            for x in range(0, len(teamA)):
+                if (x==y):
+                    continue
+                aScore += matrix[teamA[y]][teamA[x]]
+                bScore += matrix[teamB[y]][teamB[x]]
+        ans = min(abs(aScore-bScore),ans)
+        return
 
-            check[x][y] = check[y][x] = True
-            getScore(index+1,addA+1, teamA+matrix[x][y]+matrix[y][x], teamB)
-            getScore(index+1,addA, teamA,teamB+matrix[x][y]+matrix[y][x])
-            check[x][y] = check[y][x] = False
+    getScore(index+1, teamA+[index], teamB)
+    getScore(index+1, teamA, teamB+[index])
 
-getScore(0,0,0,0)
+getScore(0, [], [])
 print(ans)
