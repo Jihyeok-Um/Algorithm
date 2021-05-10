4a1f68825de8721e0a3437c32b8b9356923f7243
Izalpha <visualstudio530@naver.com>
2021-05-11 02:45:09 +0900

    16194

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 n = int(input())
 card = [0] + list(map(int, input().split()))
-d = [0]*(n+1)
+d = [1000*10000]*(n+1)
+d[0] = 0
 for i in range(1, n+1):
     for j in range(1, i+1):
-         d[i] = max(d[i], d[i-j]+card[j])
+         d[i] = min(d[i], d[i-j]+card[j])
 
 print(d[n])
