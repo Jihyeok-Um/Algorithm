0ff1f87b837fe34af562f039a1a91392e93eb3b5
Izalpha <visualstudio530@naver.com>
2021-05-10 23:04:16 +0900

    9095 (DP ver.)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-n = int(input())
-d = [0]*(n+1)
+testCase = int(input())
+d = [0]*11
 d[1] = 1
-if (n > 1):
-    d[2] = 3
+d[2] = 2
+d[3] = 4
 
-if (n >= 3):
-    for i in range(3, n+1):
-        d[i] = d[i-1] + d[i-2]*2
-
-print(d[n] % 10007)
+for i in range(testCase):
+    n = int(input())
+    if(n >= 4):
+        for i in range(4, n+1):
+            d[i] = d[i-1]+d[i-2]+d[i-3]
+    print(d[n])
