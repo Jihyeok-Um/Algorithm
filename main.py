6d11d89c2825c0d331a88d986b5937b4c7d07bb0
Izalpha <visualstudio530@naver.com>
2021-05-10 22:30:17 +0900

    11726

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 n = int(input())
 d = [0]*(n+1)
-d[1] = 0
-for i in range(2, n+1):
-    d[i] = d[i-1] + 1
-    if i%2 == 0 and d[i] >= d[i//2] + 1:
-        d[i] = d[i//2] + 1
-    if i%3 == 0 and d[i] >= d[i//3] + 1:
-        d[i] = d[i//3] + 1
-print(d[n])
+d[1] = 1
+if (n > 1):
+    d[2] = 2
+
+if (n >= 3):
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
+
+print(d[n] % 10007)
