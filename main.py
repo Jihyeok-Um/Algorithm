21528e8b4036b437a1750e78ff8915e14fcc13b3
Izalpha <visualstudio530@naver.com>
2021-05-11 16:13:37 +0900

    15990 doneCode

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 testCase = int(input())
-d = [[0]*4 for _ in range(100001)]
+mod = 1000000009
+max_ = 100001
+d = [[0]*4 for _ in range(max)]
 d[1][1] = d[2][2] = d[3][1] = d[3][2] = d[3][3] = 1
 
-for i in range(4,100001):
+for i in range(4,max):
     d[i][1] = d[i-1][2] + d[i-1][3]
     d[i][2] = d[i-2][1] + d[i-2][3]
     d[i][3] = d[i-3][1] + d[i-3][2]
+    d[i][1] %= mod
+    d[i][2] %= mod
+    d[i][3] %= mod
 
 for i in range(testCase):
     n = int(input())
-    print(sum(d[n])%1000000009)
+    print(sum(d[n])%mod)
