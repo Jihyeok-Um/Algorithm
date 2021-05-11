6a3026273eaec9ebe394e1719aafba9ae57ffd43
Izalpha <visualstudio530@naver.com>
2021-05-11 16:06:22 +0900

    15990 test

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-n = int(input())
-card = [0] + list(map(int, input().split()))
-d = [1000*10000]*(n+1)
-d[0] = 0
-for i in range(1, n+1):
-    for j in range(1, i+1):
-         d[i] = min(d[i], d[i-j]+card[j])
+testCase = int(input())
+d = [[0]*4 for _ in range(100001)]
+d[1][1] = d[2][2] = d[3][1] = d[3][2] = d[3][3] = 1
 
-print(d[n])
+for i in range(4,100001):
+    d[i][1] = d[i-1][2] + d[i-1][3]
+    d[i][2] = d[i-2][1] + d[i-2][3]
+    d[i][3] = d[i-3][1] + d[i-3][2]
+
+for i in range(testCase):
+    n = int(input())
+    print(sum(d[n])%1000000009)
