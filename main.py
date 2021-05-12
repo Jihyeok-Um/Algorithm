b03540b4e31a1e2951e4cdf685b08a71f198e7eb
Izalpha <visualstudio530@naver.com>
2021-05-12 17:08:27 +0900

    2193

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 n = int(input())
-mod = 1000000000
-d = [[0]*10 for i in range(n+1)]
-d[1][1] = d[1][2] = d[1][3] = d[1][4] = d[1][5] = d[1][6] = d[1][7] = d[1][8] = d[1][9] = 1
+d = [[0]*2 for i in range(n+1)]
+d[1][1] = 1
 
 for i in range(2,n+1):
-    for j in range(0,10):
-        if (j != 0 and j != 9):
-            d[i][j] = d[i-1][j+1] + d[i-1][j-1]
-        elif (j == 0):
-            d[i][j] = d[i-1][j+1]
-        elif (j == 9):
-            d[i][j] = d[i-1][j-1]
+    d[i][0] = d[i-1][0] + d[i-1][1]
+    d[i][1] = d[i-1][0]
 
-print(sum(d[n]) % 1000000000)
+print(sum(d[n]))
