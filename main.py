25e038dced4fb858d45d9541da0c36acbc03c740
Izalpha <visualstudio530@naver.com>
2021-05-16 03:15:46 +0900

    9461

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-n = int(input())
-d = list(map(int,input().split()))
-length = [1 for i in range(n)]
+testCase = int(input())
+last = 101
+d = [0 for i in range(last)]
+d[1] = d[2] = d[3] = 1
+d[4] = d[5] = 2
 
-for i in range(1, len(d)):
-    for j in range(0, i):
-        if(d[i] > d[j] and length[i] <= length[j]+1):
-            length[i] = length[j]+1
+for i in range(6,last):
+    d[i] = d[i-1] + d[i-5]
 
-print(max(length))
+for i in range(testCase):
+    temp = int(input())
+    print(d[temp])
