c7eb66581ef5586a8973f0bf4373c05b732ccc34
Izalpha <visualstudio530@naver.com>
2021-05-16 01:09:27 +0900

    1427

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = int(input())
-d = [0 for i in range(1000001)]
-d[1] = 1
-d[2] = 2
-for i in range(3,1000001):
-    d[i] = d[i-1] + d[i-2]
-    d[i] %= 15746
-print(d[n])
+d = list(input())
+d.sort(reverse=True)
+
+for i in d:
+    print(i, end='')
