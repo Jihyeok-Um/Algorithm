71030bcadcc0efab08bbbce421dbeeda97afc849
Izalpha <visualstudio530@naver.com>
2021-05-16 02:10:46 +0900

    11053

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-d = list(input())
-d.sort(reverse=True)
+n = int(input())
+d = list(map(int,input().split()))
+length = [1 for i in range(n)]
 
-for i in d:
-    print(i, end='')
+for i in range(1, len(d)):
+    for j in range(0, i):
+        if(d[i] > d[j] and length[i] <= length[j]+1):
+            length[i] = length[j]+1
+
+print(max(length))
