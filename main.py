aad3695a1455f7c7eec0eaa7fcd85628f8b9e83a
Izalpha <visualstudio530@naver.com>
2021-05-19 04:01:22 +0900

    1699

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 n = int(input())
-d = list(map(int, input().split()))
-s = [0 for i in range(n)]
+d = [100000 for i in range(n+1)]
+d[0] = 0
+d[1] = 1
 
-for i in range(0,n):
-    if i == 0:
-        s[i] = d[i]
-    else:
-        s[i] = max(d[i],s[i-1]+d[i])
+for i in range(1,n+1):
+    j = 1
+    while (j*j <= i):
+        if (d[i] > d[i-(j*j)]+1):
+            d[i] = d[i-(j*j)]+1
+        j += 1
 
-print(max(s))
+print(d[n])
