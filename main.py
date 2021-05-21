25d5e22ba3a1a3cb8e97d39a02c5e968930db671
Izalpha <visualstudio530@naver.com>
2021-05-21 23:13:36 +0900

    1149

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 n = int(input())
-red = []
-green = []
-blue = []
+red = [0]
+green = [0]
+blue = [0]
+d = [[0 for i in range(3)] for i in range(n+1)]
 temp1 = temp2 = temp3 = 0
 for i in range(n):
     temp1,temp2,temp3 = map(int, input().split())
@@ -9,3 +10,11 @@ for i in range(n):
     green.append(temp2)
     blue.append(temp3)
 
+i = 1
+while(i <= n):
+    d[i][0] = min(d[i - 1][1], d[i - 1][2]) + red[i]
+    d[i][1] = min(d[i - 1][0], d[i - 1][2]) + green[i]
+    d[i][2] = min(d[i - 1][0], d[i - 1][1]) + blue[i]
+    i += 1
+
+print(min(d[n][0],d[n][1],d[n][2]))
