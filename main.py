823d5fd1637b558fd1601b8ad3bea8fc8a7d435e
Izalpha <visualstudio530@naver.com>
2021-05-21 22:02:56 +0900

    1149 중간과정

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-testCase = int(input())
-mod = 1000000009
-d = [0]*1000001
-d[1] = 1
-d[2] = 2
-d[3] = 4
+n = int(input())
+red = []
+green = []
+blue = []
+temp1 = temp2 = temp3 = 0
+for i in range(n):
+    temp1,temp2,temp3 = map(int, input().split())
+    red.append(temp1)
+    green.append(temp2)
+    blue.append(temp3)
 
-for i in range(testCase):
-    n = int(input())
-    if(n >= 4):
-        for i in range(4, n+1):
-            d[i] = d[i-1]+d[i-2]+d[i-3]
-            d[i] %= mod
-    print(d[n] % mod)
