c883424c07ae4b93e26dd1113ee8c838e5333457
Izalpha <visualstudio530@naver.com>
2021-05-22 03:55:51 +0900

    11057

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 n = int(input())
-d = [[0]*3 for _ in range(n+1)]
-d[0][0] = 1
-for i in range(1, n+1):
-    d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2]
-    d[i][1] = d[i-1][0] + d[i-1][2]
-    d[i][2] = d[i-1][0] + d[i-1][1]
-    for j in range(3):
-        d[i][j] %= 9901
+mod = 10007
+d = [[0 for i in range(10)] for i in range(n)]
+for i in range(0,10):
+    d[0][i] = 1
 
-print(sum(d[n]) % 9901)
+for i in range(1,n):
+    for j in range(0,10):
+        k = 0
+        while(k <= j):
+            d[i][j] += d[i-1][k]
+            k += 1
+            d[i][j] %= 10007
+
+
+print((sum(d[n-1])) % 10007)
