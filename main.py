2aa8c978917f1564287a46db981188bfbcde1ff5
Izalpha <visualstudio530@naver.com>
2021-05-22 02:46:10 +0900

    1309

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 n = int(input())
-red = [0]
-green = [0]
-blue = [0]
-d = [[0 for i in range(3)] for i in range(n+1)]
-temp1 = temp2 = temp3 = 0
-for i in range(n):
-    temp1,temp2,temp3 = map(int, input().split())
-    red.append(temp1)
-    green.append(temp2)
-    blue.append(temp3)
+d = [[0]*3 for _ in range(n+1)]
+d[0][0] = 1
+for i in range(1, n+1):
+    d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2]
+    d[i][1] = d[i-1][0] + d[i-1][2]
+    d[i][2] = d[i-1][0] + d[i-1][1]
+    for j in range(3):
+        d[i][j] %= 9901
 
-i = 1
-while(i <= n):
-    d[i][0] = min(d[i - 1][1], d[i - 1][2]) + red[i]
-    d[i][1] = min(d[i - 1][0], d[i - 1][2]) + green[i]
-    d[i][2] = min(d[i - 1][0], d[i - 1][1]) + blue[i]
-    i += 1
-
-print(min(d[n][0],d[n][1],d[n][2]))
+print(sum(d[n]) % 9901)
