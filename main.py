4d2e8201f7fa9a1bbab1a140f41ab0bacc86011a
Izalpha <visualstudio530@naver.com>
2021-05-21 21:49:16 +0900

    15988

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-arr = list(map(int, input().split()))
-ans = [0 for i in range(101)]
-output = []
+testCase = int(input())
+mod = 1000000009
+d = [0]*1000001
+d[1] = 1
+d[2] = 2
+d[3] = 4
 
-for i in range(len(arr)):
-    ans[arr[i]] = arr.count(arr[i])
-
-for i in range(len(ans)):
-    if (ans[i] != 0 and ans[i] != 1):
-        output.append(ans[i])
-
-print(output)
+for i in range(testCase):
+    n = int(input())
+    if(n >= 4):
+        for i in range(4, n+1):
+            d[i] = d[i-1]+d[i-2]+d[i-3]
+            d[i] %= mod
+    print(d[n] % mod)
