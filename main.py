802d902e5f0bd4740bc94e2ab98ce574fb56c71a
Izalpha <visualstudio530@naver.com>
2021-05-24 00:24:54 +0900

    2156 끝

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ for i in range(n):
     arr.append(int(input()))
 
 d[1][1] = arr[1]
-d[2][2] = arr[1]+arr[2]
+if (n >= 2):
+    d[2][2] = arr[1]+arr[2]
 
-for i in range(3,n+1):
-    d[i][0] = max(d[i-1]) #0번
-    d[i][1] = max(d[i-2])+arr[i] #1번
-    d[i][2] = max(d[i-3])+arr[i-i]+arr[i] #2번
+if (n >= 3):
+    for i in range(3,n+1):
+        d[i][0] = max(d[i-1]) #0번
+        d[i][1] = max(d[i-2])+arr[i] #1번
+        d[i][2] = max(d[i-3])+arr[i-1]+arr[i] #2번
 
 print(max(d[n]))
