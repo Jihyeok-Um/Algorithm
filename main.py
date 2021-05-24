abc011a063e4df8dc06c1fc01442b7b09cfb5a75
Izalpha <visualstudio530@naver.com>
2021-05-25 08:25:59 +0900

    1932

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 n = int(input())
-arr = [0]
-d = [[0 for i in range(3)] for i in range(n+1)]
+a = []
+d = [[0 for i in range(n)] for i in range(n)]
 for i in range(n):
-    arr.append(int(input()))
+    a.append(list(map(int,input().split())))
+d[0][0] = a[0][0]
 
-d[1][1] = arr[1]
-if (n >= 2):
-    d[2][2] = arr[1]+arr[2]
+for i in range(1,n):
+    for j in range(0,i+1):
+        d[i][j] = max(d[i-1][j-1],d[i-1][j])+a[i][j]
 
-if (n >= 3):
-    for i in range(3,n+1):
-        d[i][0] = max(d[i-1]) #0번
-        d[i][1] = max(d[i-2])+arr[i] #1번
-        d[i][2] = max(d[i-3])+arr[i-1]+arr[i] #2번
-
-print(max(d[n]))
+print(max(d[n-1]))
