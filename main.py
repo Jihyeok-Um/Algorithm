79b4bc648c0d8bc27e4ae2434c802262d939d0c9
Izalpha <visualstudio530@naver.com>
2021-05-26 22:46:15 +0900

    11054

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 n = int(input())
 d = list(map(int,input().split()))
-sum = list(d)
+length = [1 for i in range(n)]
+ans = 0
 
 for i in range(1, len(d)):
     for j in range(0, i):
-        if(d[i] > d[j] and sum[j]+d[i] > sum[i]):
-            sum[i] = sum[j]+d[i]
+        if(d[i] > d[j] and length[i] <= length[j]+1):
+            length[i] = length[j]+1
 
-print(max(sum))
+copy = list(length)
+
+for i in range(0, len(d)):
+    for j in range(i, len(d)):
+        if(d[i] > d[j] and copy[j] <= copy[i]+1):
+            copy[j] = copy[i]+1
+
+ans = max(copy)
+
+print(max(max(length),ans))
+
