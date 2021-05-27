cb2d4e73609030edbde032437a2592628d182ec6
Izalpha <visualstudio530@naver.com>
2021-05-28 04:40:48 +0900

    9184

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+d = [[[0 for i in range(21)] for i in range(21)] for i in range(21)]
+
 def w(a,b,c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
     if a > 20 or b > 20 or c > 20:
-        return w(20,20,20)
-    if a < b and b < c:
-        return w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c)
+        d[20][20][20] = w(20,20,20)
+        return d[20][20][20]
 
-    return w(a-1,b,c) + w(a-1, b-1, c) + w(a-1, b ,c-1) - w(a-1, b-1, c-1)
+    if (d[a][b][c] != 0):
+        return d[a][b][c]
+
+    if a < b and b < c:
+        d[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c)
+        return d[a][b][c]
+
+    d[a][b][c] = w(a-1,b,c) + w(a-1, b-1, c) + w(a-1, b ,c-1) - w(a-1, b-1, c-1)
+    return d[a][b][c]
 
 while(True):
     a,b,c = map(int,input().split())
