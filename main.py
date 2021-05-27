377fc8956d11822e0f81684e6cef93a4fb5448b8
Izalpha <visualstudio530@naver.com>
2021-05-28 00:29:40 +0900

    2133

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 n = int(input())
-d = list(map(int, input().split()))
-s = [0 for i in range(n)]
-s2 = [0 for i in range(n)]
-ans = -1001
-for i in range(0,n):
-    if i == 0:
-        s[i] = d[i]
-    else:
-        s[i] = max(d[i],s[i-1]+d[i])
+d = [0 for i in range(n+1)]
+d[0] = 1
 
-for i in range(n-1, -1, -1):
-    if i == n-1:
-        s2[i] = d[i]
-    else:
-        s2[i] = max(d[i],s2[i+1]+d[i])
+if (n >= 2):
+    for i in range(2,n+1):
+        d[i] = d[i-2]*3
+        for j in range(i-4,-2,-2):
+            d[i] += d[j]*2
 
-for i in range(1,n-1):
-    if (s[i-1] + s2[i+1] > ans):
-        ans = s[i-1] + s2[i+1]
-
-print(max(ans,max(s2),max(s)))
+print(d[n])
