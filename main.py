cb0af756f52903850aca8e2c2c2bb12a546ebb57
Izalpha <visualstudio530@naver.com>
2021-05-29 11:37:05 +0900

    11051

--- main.py
+++ main.py
@@ -1,57 +1,12 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-queue = [0 for i in range(n)]
-start = 0
-end = 0
+n, k = map(int, input().split())
+d = [[0 for i in range(n+1)] for i in range(n+1)]
+for i in range(0, n+1):
+    for j in range(0, i + 1):
+        if (j == 0):
+            d[i][j] = 1
+        elif (j == i):
+            d[i][j] = 1
+        elif (j != 0 and j != i):
+            d[i][j] = d[i-1][j-1] + d[i-1][j]
 
-def push(num):
-    global end
-    queue[end] = int(num)
-    end += 1
-
-def pop():
-    global start
-    if(queue[start] == 0):
-        print(-1)
-    else:
-        print(queue[start])
-        queue[start] = 0
-        start += 1
-
-def size():
-    print(end - start)
-
-def empty():
-    if(start == end):
-        print(1)
-    else:
-        print(0)
-
-def front():
-    if(start == end):
-        print(-1)
-    else:
-        print(queue[start])
-
-def back():
-    if(start == end):
-        print(-1)
-    else:
-        print(queue[end-1])
-
-for i in range(n):
-    d = list(map(str,input().split()))
-    if(d[0] == 'push'):
-        push(d[1])
-    elif(d[0] == 'pop'):
-        pop()
-    elif(d[0] == 'size'):
-        size()
-    elif(d[0] == 'empty'):
-        empty()
-    elif(d[0] == 'front'):
-        front()
-    elif(d[0] == 'back'):
-        back()
-    d = []
+print(d[n][k] % 10007)
