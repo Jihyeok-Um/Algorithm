08163b855b2f74f0ff5b85818feb9b3962c0c67a
Izalpha <visualstudio530@naver.com>
2021-05-31 16:42:01 +0900

    DFS와 BFS (인접리스트로 구현)

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-node,edge,vertex = map(int, input().split())
-matrix = [[0]*node for i in range(node)]
-for i in range(edge):
-    a,b = map(int, input().split())
-    matrix[a-1][b-1] = matrix[b-1][a-1] = 1
-visitList = [0] * (node)
+from collections import deque
 
-def dfs(vertex):
-    visitList[vertex]=1
-    print(vertex+1, end=' ')
-    for i in range(0, node):
-        if(visitList[i]==0 and matrix[vertex][i]==1):
+n,m,v = map(int,input().split())
+matrix = [[] for i in range(n+1)]
+check = [False for i in range(n+1)]
+for i in range(m):
+    x,y = map(int,input().split())
+    matrix[x].append(y)
+    matrix[y].append(x)
+
+def dfs(v):
+    check[v] = True
+    print(v,end=' ')
+    matrix[v].sort()
+    for i in matrix[v]:
+        if(check[i] == False):
             dfs(i)
 
+def bfs(v):
+    check = [False for i in range(n + 1)]
+    q = deque()
+    q.append(v)
+    check[v] = True
+    while(q):
+        x = q.popleft()
+        print(x,end=' ')
+        for i in matrix[x]:
+            if(check[i] == False):
+                check[i] = True
+                q.append(i)
 
-def bfs(vertex):
-    queue=[vertex]
-    visitList[vertex]=1
-    while queue:
-        vertex=queue.pop(0)
-        print(vertex+1, end=' ')
-        for i in range(0, node):
-            if(visitList[i]==0 and matrix[vertex][i]==1):
-                queue.append(i)
-                visitList[i]=1
 
-dfs(vertex - 1)
-visitList = [0] * (node+1)
+dfs(v)
 print()
-bfs(vertex - 1)
+bfs(v)
