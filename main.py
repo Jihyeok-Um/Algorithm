f98dfc2826f0a2d79e059cf21c2da6b58003cdca
Izalpha <visualstudio530@naver.com>
2021-05-31 18:12:47 +0900

    2178

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 from collections import deque
 
-n,m,v = map(int,input().split())
-matrix = [[] for i in range(n+1)]
-check = [False for i in range(n+1)]
-for i in range(m):
-    x,y = map(int,input().split())
-    matrix[x].append(y)
-    matrix[y].append(x)
+dy = [0,-1,0,1]
+dx = [1,0,-1,0]
+n,m = map(int,input().split())
+matrix = []
+check = [[-1 for i in range(m)] for i in range(n)]
+for i in range(n):
+    temp = input()
+    small = []
+    for i in range(0,m):
+        small.append(int(temp[i]))
+    matrix.append(small)
 
-def dfs(v):
-    check[v] = True
-    print(v,end=' ')
-    matrix[v].sort()
-    for i in matrix[v]:
-        if(check[i] == False):
-            dfs(i)
-
-def bfs(v):
-    check = [False for i in range(n + 1)]
+def bfs(x,y):
     q = deque()
+    v = [x,y]
     q.append(v)
-    check[v] = True
+    check[y][x] = 1
     while(q):
-        x = q.popleft()
-        print(x,end=' ')
-        for i in matrix[x]:
-            if(check[i] == False):
-                check[i] = True
-                q.append(i)
+        temp = q.popleft()
+        for i in range(0,4):
+            if (temp[0]+dy[i] >= 0 and temp[0]+dy[i] < n and temp[1]+dx[i] >=0 and temp[1]+dx[i] < m):
+                if (matrix[temp[0]+dy[i]][temp[1]+dx[i]] == 1 and check[temp[0]+dy[i]][temp[1]+dx[i]] == -1):
+                    q.append([temp[0]+dy[i],temp[1]+dx[i]])
+                    check[temp[0] + dy[i]][temp[1] + dx[i]] = check[temp[0]][temp[1]]+1
 
-
-dfs(v)
-print()
-bfs(v)
+bfs(0,0)
+print(check[n-1][m-1])
