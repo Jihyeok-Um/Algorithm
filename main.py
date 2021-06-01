9eb8d9eacd5d97718f713af031f024dd897a0421
Izalpha <visualstudio530@naver.com>
2021-06-01 12:37:18 +0900

    7569

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 from collections import deque
 q = deque()
-dx = [1,0,-1,0]
-dy = [0,-1,0,1]
 ans = 0
 
-m,n = map(int,input().split())
+m,n,h = map(int,input().split())
 matrix = []
-days = [[-1 for i in range(m)] for i in range(n)]
-for i in range(n):
-    matrix.append(list(map(int,input().split())))
+small = []
+dx = [1,0,-1,0,0,0]
+dy = [0,-1,0,1,0,0]
+dz = [0,0,0,0,1,-1]
+days = [[[-1 for i in range(m)] for i in range(n)] for i in range(h)]
+for j in range(h):
+    for i in range(n):
+        small.append(list(map(int,input().split())))
+    matrix.append(small)
+    small = []
 
-for y in range(n):
-    for x in range(m):
-        if(matrix[y][x] == 1):
-            q.append([x,y])
-            days[y][x] = 0
-        elif(matrix[y][x] == -1):
-            days[y][x] = -2
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if(matrix[z][y][x] == 1):
+                q.append([x,y,z])
+                days[z][y][x] = 0
+            elif(matrix[z][y][x] == -1):
+                days[z][y][x] = -2
 
 while(q):
     temp = q.popleft()
-    for i in range(0,4):
-        if(temp[0]+dx[i] >= 0 and temp[0]+dx[i] < m and temp[1]+dy[i] >=0 and temp[1]+dy[i] < n and days[temp[1]+dy[i]][temp[0]+dx[i]] == -1):
-            if(matrix[temp[1]+dy[i]][temp[0]+dx[i]] != -1):
-                ans = days[temp[1]+dy[i]][temp[0]+dx[i]] = days[temp[1]][temp[0]]+1
-                q.append([temp[0]+dx[i],temp[1]+dy[i]])
+    for i in range(0,6):
+        x = temp[0] + dx[i]
+        y = temp[1] + dy[i]
+        z = temp[2] + dz[i]
+        if (x >= 0 and x < m and y >= 0 and y < n and z >= 0 and z < h and days[z][y][x] == -1):
+            if(matrix[z][y][x] != -1):
+                ans = days[z][y][x] = days[temp[2]][temp[1]][temp[0]]+1
+                q.append([x,y,z])
 
 check = 0
 
-for y in range(n):
-    for x in range(m):
-        if(days[y][x] == -1):
-            check = -1
-            break
-        else:
-            ans = max(ans,days[y][x])
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if(days[z][y][x] == -1):
+                check = -1
+                break
+            else:
+                ans = max(ans,days[z][y][x])
 
 if (check == -1):
     print(check)
