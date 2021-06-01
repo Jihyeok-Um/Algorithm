2f9f3827980e59713463aa8f28f9798b6b47419f
Izalpha <visualstudio530@naver.com>
2021-06-01 20:35:19 +0900

    7562 - 나이트의 이동

--- main.py
+++ main.py
@@ -1,52 +1,32 @@
 from collections import deque
 q = deque()
-ans = 0
+t = int(input())
+dy = [2,1,-1,-2,-2,-1,1,2]
+dx = [1,2,2,1,-1,-2,-2,-1]
 
-m,n,h = map(int,input().split())
-matrix = []
-small = []
-dx = [1,0,-1,0,0,0]
-dy = [0,-1,0,1,0,0]
-dz = [0,0,0,0,1,-1]
-days = [[[-1 for i in range(m)] for i in range(n)] for i in range(h)]
-for j in range(h):
-    for i in range(n):
-        small.append(list(map(int,input().split())))
-    matrix.append(small)
-    small = []
+for i in range(t):
+    ans = 0
+    l = int(input())
+    startX, startY = map(int,input().split())
+    finishX, finishY = map(int,input().split())
+    if(startX == finishX and startY == finishY):
+        print(ans)
+        continue
+    matrix = [[-1 for i in range(l)] for i in range(l)]
 
-for z in range(h):
-    for y in range(n):
-        for x in range(m):
-            if(matrix[z][y][x] == 1):
-                q.append([x,y,z])
-                days[z][y][x] = 0
-            elif(matrix[z][y][x] == -1):
-                days[z][y][x] = -2
-
-while(q):
-    temp = q.popleft()
-    for i in range(0,6):
-        x = temp[0] + dx[i]
-        y = temp[1] + dy[i]
-        z = temp[2] + dz[i]
-        if (x >= 0 and x < m and y >= 0 and y < n and z >= 0 and z < h and days[z][y][x] == -1):
-            if(matrix[z][y][x] != -1):
-                ans = days[z][y][x] = days[temp[2]][temp[1]][temp[0]]+1
-                q.append([x,y,z])
-
-check = 0
-
-for z in range(h):
-    for y in range(n):
-        for x in range(m):
-            if(days[z][y][x] == -1):
-                check = -1
-                break
-            else:
-                ans = max(ans,days[z][y][x])
-
-if (check == -1):
-    print(check)
-else:
+    q.append([startX,startY])
+    matrix[startY][startX] = 0
+    while(q):
+        temp = q.popleft()
+        for i in range(len(dx)):
+            x = temp[0]+dx[i]
+            y = temp[1]+dy[i]
+            if (x >= 0 and x < l and y >= 0 and y < l and matrix[y][x] == -1):
+                if (x == finishX and y == finishY):
+                    ans = matrix[temp[1]][temp[0]]+1
+                    q.clear()
+                    break
+                else:
+                    q.append([x,y])
+                    matrix[y][x] = matrix[temp[1]][temp[0]] + 1
     print(ans)
