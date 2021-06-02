95f38f05b07bb6687d6675733707e5c2a6b05fcf
Izalpha <visualstudio530@naver.com>
2021-06-02 20:30:53 +0900

    1697 - 숨바꼭질

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 from collections import deque
+n,k = map(int,input().split())
+matrix = [-1 for i in range(200000)]
 q = deque()
-t = int(input())
-dy = [2,1,-1,-2,-2,-1,1,2]
-dx = [1,2,2,1,-1,-2,-2,-1]
 
-for i in range(t):
-    ans = 0
-    l = int(input())
-    startX, startY = map(int,input().split())
-    finishX, finishY = map(int,input().split())
-    if(startX == finishX and startY == finishY):
-        print(ans)
-        continue
-    matrix = [[-1 for i in range(l)] for i in range(l)]
-
-    q.append([startX,startY])
-    matrix[startY][startX] = 0
-    while(q):
-        temp = q.popleft()
-        for i in range(len(dx)):
-            x = temp[0]+dx[i]
-            y = temp[1]+dy[i]
-            if (x >= 0 and x < l and y >= 0 and y < l and matrix[y][x] == -1):
-                if (x == finishX and y == finishY):
-                    ans = matrix[temp[1]][temp[0]]+1
-                    q.clear()
-                    break
-                else:
-                    q.append([x,y])
-                    matrix[y][x] = matrix[temp[1]][temp[0]] + 1
-    print(ans)
+q.append(n)
+matrix[n] = 0
+while(q):
+    x = q.popleft()
+    if(x == k):
+        print(matrix[x])
+        break
+    if (x-1 >= 0 and matrix[x-1] == -1):
+        q.append(x-1)
+        matrix[x-1] = matrix[x] + 1
+    if (x+1 < 200000 and matrix[x+1] == -1):
+        q.append(x+1)
+        matrix[x+1] = matrix[x] + 1
+    if (x*2 < 200000 and matrix[x*2] == -1):
+        q.append(x*2)
+        matrix[x*2] = matrix[x] + 1
