3f7558242165c14d3e8b9299ae3a2438cd6b8215
Izalpha <visualstudio530@naver.com>
2021-06-03 10:30:55 +0900

    13913 - 숨바꼭질4

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from collections import deque
 n,k = map(int,input().split())
 matrix = [-1 for i in range(200000)]
+trace = [-1 for i in range(200000)]
+ans = []
 q = deque()
 
 q.append(n)
 matrix[n] = 0
+trace[n] = -2
 while(q):
     x = q.popleft()
     if(x == k):
@@ -13,9 +16,23 @@ while(q):
     if (x-1 >= 0 and matrix[x-1] == -1):
         q.append(x-1)
         matrix[x-1] = matrix[x] + 1
+        trace[x-1] = x
     if (x+1 < 200000 and matrix[x+1] == -1):
         q.append(x+1)
         matrix[x+1] = matrix[x] + 1
+        trace[x+1] = x
     if (x*2 < 200000 and matrix[x*2] == -1):
         q.append(x*2)
         matrix[x*2] = matrix[x] + 1
+        trace[x*2] = x
+
+while(True):
+    if(trace[x] == -2):
+        ans.append(x)
+        break
+    else:
+        ans.append(x)
+        x = trace[x]
+
+for i in range(len(ans)-1,-1,-1):
+    print(ans[i],end=' ')
