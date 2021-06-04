8a0e9995779ebcedc4dfcc2187bed03419f80da0
Izalpha <visualstudio530@naver.com>
2021-06-05 02:12:41 +0900

    13549 - 숨바꼭질 3

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from collections import deque
-s = int(input())
-matrix = [[-1 for i in range(s*2)] for i in range(s*2)]
+n,k = map(int,input().split())
+matrix = [-1 for i in range(200000)]
 q = deque()
-matrix[1][0] = 0
-q.append([1,0])
 
-while(max(matrix[s]) == -1):
-    temp = q.popleft()
-    if (temp[0] >= 1 and temp[0] <= s*2 and matrix[temp[0]][temp[0]] == -1):
-        matrix[temp[0]][temp[0]] = matrix[temp[0]][temp[1]] + 1
-        q.append([temp[0],temp[0]])
-    if (temp[0] >= 1 and temp[0]+temp[1] <= s*2 and matrix[temp[0]+temp[1]][temp[1]] == -1):
-        matrix[temp[0]+temp[1]][temp[1]] = matrix[temp[0]][temp[1]] + 1
-        q.append([temp[0]+temp[1],temp[1]])
-    if (temp[0]-1 >= 1 and temp[0]-1 <= s*2 and matrix[temp[0]-1][temp[1]] == -1):
-        matrix[temp[0]-1][temp[1]] = matrix[temp[0]][temp[1]] + 1
-        q.append([temp[0]-1,temp[1]])
-
-print(max(matrix[s]))
+q.append(n)
+matrix[n] = 0
+while(q):
+    x = q.popleft()
+    if(x == k):
+        print(matrix[x])
+        break
+    if (x*2 < 200000 and matrix[x*2] == -1):
+        q.append(x*2)
+        matrix[x*2] = matrix[x]
+    if (x-1 >= 0 and matrix[x-1] == -1):
+        q.append(x-1)
+        matrix[x-1] = matrix[x] + 1
+    if (x+1 < 200000 and matrix[x+1] == -1):
+        q.append(x+1)
+        matrix[x+1] = matrix[x] + 1
