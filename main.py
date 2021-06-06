8ca333d78630a14485a77488131735a76c04176e
Izalpha <visualstudio530@naver.com>
2021-06-06 23:34:45 +0900

    1261 - 알고스팟

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from collections import deque
-n,k = map(int,input().split())
-matrix = [-1 for i in range(200000)]
+n,m = map(int,input().split())
+matrix = [list(map(int,list(input()))) for i in range(m)]
+check = [[-1 for i in range(n)] for i in range(m)]
+dx = [1,0,-1,0]
+dy = [0,-1,0,1]
 q = deque()
+q2 = deque()
+q.append([0,0])
+check[0][0] = 0
+while(q or q2):
+    if (q):
+        a = 1 #의미없음
+    elif (q2):
+        q = deque(q2)
+        q2.clear()
+    t = q.popleft()
+    for i in range(4):
+        x = t[0] + dx[i]
+        y = t[1] + dy[i]
+        if (x>=0 and x<n and y>=0 and y<m and matrix[y][x] == 1 and check[y][x] == -1):
+            check[y][x] = check[t[1]][t[0]] + 1
+            q2.append([x,y])
+        if (x>=0 and x<n and y>=0 and y<m and matrix[y][x] == 0 and check[y][x] == -1):
+            check[y][x] = check[t[1]][t[0]]
+            q.append([x,y])
 
-q.append(n)
-matrix[n] = 0
-while(q):
-    x = q.popleft()
-    if(x == k):
-        print(matrix[x])
-        break
-    if (x*2 < 200000 and matrix[x*2] == -1):
-        q.append(x*2)
-        matrix[x*2] = matrix[x]
-    if (x-1 >= 0 and matrix[x-1] == -1):
-        q.append(x-1)
-        matrix[x-1] = matrix[x] + 1
-    if (x+1 < 200000 and matrix[x+1] == -1):
-        q.append(x+1)
-        matrix[x+1] = matrix[x] + 1
+for i in range(m):
+    print(check[i])
+
+print(check[m-1][n-1])
