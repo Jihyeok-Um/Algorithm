e69b6d332ab732cc9de20a6eff216d33562b91e1
Izalpha <visualstudio530@naver.com>
2021-06-08 23:02:57 +0900

    프로그래머스 - 모의고사

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-from collections import deque
+def solution(answers):
+    num1 = [1, 2, 3, 4, 5]
+    num2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cmt = [0, 0, 0]
+    for i in range(0, len(answers)):
+        if (answers[i] == num1[i % 5]):
+            cmt[0] += 1
+        if (answers[i] == num2[i % 8]):
+            cmt[1] += 1
+        if (answers[i] == num3[i % 10]):
+            cmt[2] += 1
 
-def solution(n, edge):
-    q = deque()
-    matrix = [[] for i in range(n + 1)]
-    check = [-1 for i in range(n + 1)]
-    for i in range(len(edge)):
-        matrix[edge[i][0]].append(edge[i][1])
-        matrix[edge[i][1]].append(edge[i][0])
-
-    q.append(1)
-    check[1] = 0
-    while (q):
-        vertex = q.popleft()
-        for j in range(0,len(matrix[vertex])):
-            if (check[matrix[vertex][j]] == -1):
-                check[matrix[vertex][j]] = check[vertex]+1
-                q.append(matrix[vertex][j])
-
-    temp = max(check)
-    ans = 0
-    for i in range(len(check)):
-        if (check[i] == temp):
-            ans += 1
+    temp = max(cmt)
+    ans = []
+    for i in range(len(cmt)):
+        if (cmt[i] == temp):
+            ans.append(i + 1)
 
     return ans
