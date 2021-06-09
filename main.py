1f3bd7102e4ea4bead6f3383b7ba3aa2676f85d6
Izalpha <visualstudio530@naver.com>
2021-06-09 13:10:40 +0900

    프로그래머스 - 순위 (푸는 중)

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-def solution(answers):
-    num1 = [1, 2, 3, 4, 5]
-    num2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cmt = [0, 0, 0]
-    for i in range(0, len(answers)):
-        if (answers[i] == num1[i % 5]):
-            cmt[0] += 1
-        if (answers[i] == num2[i % 8]):
-            cmt[1] += 1
-        if (answers[i] == num3[i % 10]):
-            cmt[2] += 1
+def solution(n, results):
+    matrix = [[] for i in range(n + 1)]
+    for i in range(n + 1):
+        for j in range(len(result)):
+            matrix[result[i][0]].append(result[i][1])
 
-    temp = max(cmt)
-    ans = []
-    for i in range(len(cmt)):
-        if (cmt[i] == temp):
-            ans.append(i + 1)
-
-    return ans
+    cmt = [0 for i in range(n + 1)]
+    for i in range(n + 1):
+        cmt[i] += len(matrix[i])
+        for j in range(len(matrix[i]):
+            cmt[matrix[i][j]] -= 1
