c21e74aabc5488cbcb2fc0173576b266e778695a
Izalpha <visualstudio530@naver.com>
2021-06-10 16:55:40 +0900

    프로그래머스 - 네트워크

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-ans = 0
+from collections import deque
 
-def bruteforce(numbers, current, i, target):
-    global ans
-    if (i == len(numbers)):
-        if (current == target):
-            ans += 1
-            return
+def solution(n, computers):
+    check = [False for i in range(n)]
+    count = net = 1
+    q = deque()
+    q.append(0)
+    check[0] = True
+
+    while (count < n):
+        if (q):
+            a = 1
         else:
-            return
+            for i in range(len(check)):
+                if (check[i] == False):
+                    check[i] = True
+                    q.append(i)
+                    net += 1
+                    count += 1
+                    break
 
-    bruteforce(numbers, current + numbers[i], i + 1, target)
-    bruteforce(numbers, current - numbers[i], i + 1, target)
+        x = q.popleft()
+        for i in range(len(computers[x])):
+            if (computers[x][i] == 1 and check[i] == False):
+                count += 1
+                q.append(i)
+                check[i] = True
 
-def solution(numbers, target):
-    global ans
-    bruteforce(numbers, 0, 0, target)
-    return ans
+    return net
