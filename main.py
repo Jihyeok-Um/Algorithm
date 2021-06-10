88a882d041aa988747c310c9426662798e929a2b
Izalpha <visualstudio530@naver.com>
2021-06-10 16:12:31 +0900

    프로그래머스 - 타겟 넘버

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-def solution(d, budget):
-    answer = 0
-    d.sort()
-    for i in d:
-        if (budget >= i):
-            budget -= i
-            answer += 1
+ans = 0
 
-    return answer
+def bruteforce(numbers, current, i, target):
+    global ans
+    if (i == len(numbers)):
+        if (current == target):
+            ans += 1
+            return
+        else:
+            return
+
+    bruteforce(numbers, current + numbers[i], i + 1, target)
+    bruteforce(numbers, current - numbers[i], i + 1, target)
+
+def solution(numbers, target):
+    global ans
+    bruteforce(numbers, 0, 0, target)
+    return ans
