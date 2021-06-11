14ff3ef0e91fc7c3378f788394223eb7926a8e31
Izalpha <visualstudio530@naver.com>
2021-06-12 00:59:23 +0900

    프로그래머스 - 행렬의 덧셈

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n, m = map(int, input().strip().split(' '))
-for i in range(m):
-    for j in range(n):
-        print("*",end='')
-    print()
+def solution(arr1, arr2):
+    answer = [[0 for i in range(len(arr1[0]))] for i in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr1[i])):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+
+    return answer
