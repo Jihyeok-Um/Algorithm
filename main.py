05dc6801ceb6169cbbec9c1a606798c684951b81
Izalpha <visualstudio530@naver.com>
2021-06-12 00:55:22 +0900

    프로그래머스 - 직사각형 별찍기

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-def solution(board, moves):
-    store = []
-    for i in range(len(moves)):
-        for j in range(len(board)):
-            if (board[j][moves[i] - 1] != 0):
-                store.append(board[j][moves[i] - 1])
-                board[j][moves[i] - 1] = 0
-                break
-
-    i = 1
-    result = 0
-    if (len(store) < 2):
-        return result
-    while (i < len(store)):
-        if (store[i] == store[i - 1]):
-            result += 2
-            del store[i]
-            del store[i - 1]
-            i = 0
-        i += 1
-
-    return result
+n, m = map(int, input().strip().split(' '))
+for i in range(m):
+    for j in range(n):
+        print("*",end='')
+    print()
