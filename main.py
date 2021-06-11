f61ff2475e419d8313d9727b52a705a2bfdf00bb
Izalpha <visualstudio530@naver.com>
2021-06-12 01:25:20 +0900

    프로그래머스 - 짝수와 홀수

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-a = 0
-
 def solution(num):
-    collatz(num, 0)
-    return a
-
-def collatz(num, count):
-    global a
-    if (count == 500):
-        a = -1
-        return -1
-    if (num == 1):
-        a = count
-        return
-
     if (num % 2 == 0):
-        collatz(num // 2, count + 1)
-    elif (num % 2 == 1):
-        collatz(num * 3 + 1, count + 1)
+        return "Even"
+    else:
+        return "Odd"
