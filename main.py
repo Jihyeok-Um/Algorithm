a4a336687c59a1ef6c3282580e3a123202793422
Izalpha <visualstudio530@naver.com>
2021-06-12 03:26:15 +0900

    프로그래머스 - 두 정수 사이의 합

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-def solution(s):
-    countP = 0
-    countY = 0
-
-    for i in range(len(s)):
-        if (s[i] == "p" or s[i] == "P"):
-            countP += 1
-        elif (s[i] == "y" or s[i] == "Y"):
-            countY += 1
-    if (countP == countY):
-        return True
-    else:
-        return False
+def solution(a, b):
+    result = 0
+    if (a > b):
+        a,b = b,a
+    for i in range(a,b+1):
+        result += i
+    return result
