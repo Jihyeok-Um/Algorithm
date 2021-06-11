bef73d2f892157a353146892ec2bf8b773dbb0e8
Izalpha <visualstudio530@naver.com>
2021-06-12 01:39:06 +0900

    프로그래머스 - 평균 구하기

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def solution(phone_number):
-    s = ""
-    for i in range(len(phone_number)):
-        if (i >= len(phone_number) - 4):
-            s += phone_number[i]
-        else:
-            s += "*"
+def solution(arr):
+    answer = 0
+    for i in arr:
+        answer += i
 
-    return s
+    return answer / len(arr)
