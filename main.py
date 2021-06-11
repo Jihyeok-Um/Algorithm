b6999fb6e0f5d6df9ca4d211675192e1a1ac977b
Izalpha <visualstudio530@naver.com>
2021-06-11 19:47:57 +0900

    프로그래머스 - 크레인 인형뽑기 게임

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-from collections import deque
+def solution(board, moves):
+    store = []
+    for i in range(len(moves)):
+        for j in range(len(board)):
+            if (board[j][moves[i] - 1] != 0):
+                store.append(board[j][moves[i] - 1])
+                board[j][moves[i] - 1] = 0
+                break
 
-def solution(n, computers):
-    check = [False for i in range(n)]
-    count = net = 1
-    q = deque()
-    q.append(0)
-    check[0] = True
+    i = 1
+    result = 0
+    if (len(store) < 2):
+        return result
+    while (i < len(store)):
+        if (store[i] == store[i - 1]):
+            result += 2
+            del store[i]
+            del store[i - 1]
+            i = 0
+        i += 1
 
-    while (count < n):
-        if (q):
-            a = 1
-        else:
-            for i in range(len(check)):
-                if (check[i] == False):
-                    check[i] = True
-                    q.append(i)
-                    net += 1
-                    count += 1
-                    break
-
-        x = q.popleft()
-        for i in range(len(computers[x])):
-            if (computers[x][i] == 1 and check[i] == False):
-                count += 1
-                q.append(i)
-                check[i] = True
-
-    return net
+    return result
