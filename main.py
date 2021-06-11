380eef7beb7684ef51be0f5c18fcdabf8c9c9016
Izalpha <visualstudio530@naver.com>
2021-06-12 02:18:02 +0900

    프로그래머스 - 자릿수 더하기

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 def solution(n):
     count = 1
-    num = []
-    while (n // count != 0):
+    while (n / count != 0):
         count *= 10
     count = count // 10
-    temp = count
 
+    result = 0
     while (count != 1):
-        num.append(n // count)
-        n %= count
+        result += n // count
+        n = n % count
         count = count // 10
-    num.append(n)
-    num.sort(reverse=True)
-    num2 = 0
-    for i in num:
-        num2 += i * temp
-        temp = temp // 10
-
-    return num2
-
-
+    result += n
+    return result
