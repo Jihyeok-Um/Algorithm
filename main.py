0e48982157310f5ba3aad86cec4e4bf1023d525d
Izalpha <visualstudio530@naver.com>
2021-06-12 03:45:20 +0900

    프로그래머스 - 같은 숫자는 싫어

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def solution(a, b):
-    result = 0
-    if (a > b):
-        a,b = b,a
-    for i in range(a,b+1):
-        result += i
-    return result
+def solution(arr):
+    i = 0
+    arr2 = []
+    for i in range(1, len(arr)):
+        if (arr[i] == arr[i - 1] and arr[i] != -1):
+            arr[i - 1] = -1
+
+    for i in range(len(arr)):
+        if (arr[i] != -1):
+            arr2.append(arr[i])
+
+    return arr2
