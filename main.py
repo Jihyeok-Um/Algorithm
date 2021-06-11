86d886b76dfdba2e313b9db6aa6d8347aca81c8c
Izalpha <visualstudio530@naver.com>
2021-06-12 01:37:46 +0900

    프로그래머스 - 핸드폰 번호 가리기

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def solution(x, n):
-    answer = []
-    for i in range(n):
-        answer.append(x+(x*i))
+def solution(phone_number):
+    s = ""
+    for i in range(len(phone_number)):
+        if (i >= len(phone_number) - 4):
+            s += phone_number[i]
+        else:
+            s += "*"
 
-    return answer
+    return s
