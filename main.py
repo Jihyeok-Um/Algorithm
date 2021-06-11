884ad4856b93f9a43b0d8e7fc1f3293dabcdc865
Izalpha <visualstudio530@naver.com>
2021-06-12 01:34:51 +0900

    프로그래머스 - x만큼 간격이 있는 n개의 숫자

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def solution(x, n):
     answer = []
     for i in range(n):
-        if (i != 0):
-            answer.append(x + (x * i))
-        else:
-            answer.append(x)
+        answer.append(x+(x*i))
 
     return answer
