6bcf4b9a239c395b7beeb664954e6cc5f6e53433
Izalpha <visualstudio530@naver.com>
2021-06-12 02:20:13 +0900

    프로그래머스 - 약수의 합

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 def solution(n):
-    count = 1
-    while (n / count != 0):
-        count *= 10
-    count = count // 10
-
     result = 0
-    while (count != 1):
-        result += n // count
-        n = n % count
-        count = count // 10
-    result += n
+    for i in range(1, n + 1):
+        if (n % i == 0):
+            result += i
+
     return result
