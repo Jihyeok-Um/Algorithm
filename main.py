df75eab73072d5cd0f9a481c2d5157b2b24bd150
Izalpha <visualstudio530@naver.com>
2021-06-12 02:29:05 +0900

    프로그래머스 - 문자열 다루기 기본

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-def solution(n):
-    result = 0
-    for i in range(1, n + 1):
-        if (n % i == 0):
-            result += i
+def solution(s):
+    lenCheck = False
+    numCheck = False
+    num = 0
+    if (len(s) == 4 or len(s) == 6):
+        lenCheck = True
 
-    return result
+    for i in range(len(s)):
+        for j in range(10):
+            if (ord(s[i]) == ord("0") + j):
+                num += 1
+
+    if (num == len(s)):
+        numCheck = True
+    if (numCheck == True and lenCheck == True):
+        return True
+    else:
+        return False
