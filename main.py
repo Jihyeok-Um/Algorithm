783356a753227f2c9b40643ebc2f81cd020b3f2b
Izalpha <visualstudio530@naver.com>
2021-06-13 00:12:37 +0900

    프로그래머스 - 이상한 문자 만들기

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# 문자열 내림차순으로 배치하기
+# 이상한 문자 만들기
 
 def solution(s):
-    temp = [i for i in s]
-    temp.sort(reverse=True)
+    index = 0
+    result = [0 for i in range(len(s))]
+    for i in range(len(s)):
+        if (s[i] == " "):
+            index = 0
+            result[i] = " "
+            continue
+        else:
+            for j in range(26):
+                if (index % 2 == 0):
+                    if (ord(s[i]) == ord('a') + j or ord(s[i]) == ord('A') + j):
+                        result[i] = chr(ord('A') + j)
+                elif (index % 2 == 1):
+                    if (ord(s[i]) == ord('A') + j or ord(s[i]) == ord('a') + j):
+                        result[i] = chr(ord('a') + j)
+        index += 1
+
     ans = ""
-    for i in range(len(temp)):
-        ans += temp[i]
+    for i in range(len(result)):
+        ans += result[i]
 
     return ans
