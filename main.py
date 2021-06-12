4e4d828f5a4a7ad43689ce021ddbaae89f08d560
Izalpha <visualstudio530@naver.com>
2021-06-13 01:39:17 +0900

    프로그래머스 - 가운데 글자 가져오기

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-# 시저 암호
-
-def solution(s, n):
-    result = [i for i in s]
-
-    for i in range(len(s)):
-        for j in range(26):
-            if (ord(s[i]) == ord('a') + j):
-                if (chr(ord('a') + j + n) > 'z'):
-                    result[i] = chr(ord('a') + j + n - 26)
-                else:
-                    result[i] = chr(ord('a') + j + n)
-
-            elif (ord(s[i]) == ord('A') + j):
-                if (chr(ord('A') + j + n) > 'Z'):
-                    result[i] = chr(ord('A') + j + n - 26)
-                else:
-                    result[i] = chr(ord('A') + j + n)
-
+def solution(s):
     ans = ""
-    for i in range(len(result)):
-        ans += result[i]
+    if (len(s) % 2 == 0):
+        ans += s[int(len(s) / 2) - 1]
+        ans += s[int(len(s) / 2)]
+    else:
+        ans += s[int(len(s) / 2)]
 
     return ans
