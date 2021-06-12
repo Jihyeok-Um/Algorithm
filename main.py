abc7614d1bdc52e6c4d4fc397bdc8126d0c5d529
Izalpha <visualstudio530@naver.com>
2021-06-13 00:12:58 +0900

    프로그래머스 - 자연수 뒤집어 배열로 만들기

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-# 이상한 문자 만들기
+# 자연수 뒤집어 배열로 만들기
 
-def solution(s):
-    index = 0
-    result = [0 for i in range(len(s))]
-    for i in range(len(s)):
-        if (s[i] == " "):
-            index = 0
-            result[i] = " "
-            continue
-        else:
-            for j in range(26):
-                if (index % 2 == 0):
-                    if (ord(s[i]) == ord('a') + j or ord(s[i]) == ord('A') + j):
-                        result[i] = chr(ord('A') + j)
-                elif (index % 2 == 1):
-                    if (ord(s[i]) == ord('A') + j or ord(s[i]) == ord('a') + j):
-                        result[i] = chr(ord('a') + j)
-        index += 1
+def solution(n):
+    a = str(n)
+    result = []
+    for i in a:
+        result.append(int(i))
 
-    ans = ""
-    for i in range(len(result)):
-        ans += result[i]
-
-    return ans
+    result.reverse()
+    return result
