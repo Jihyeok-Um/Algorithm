c0024810688c769e0a3126238bb7434d3ef87035
Izalpha <visualstudio530@naver.com>
2021-06-13 00:11:52 +0900

    프로그래머스 - 나누어 떨어지는 숫자 배열

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-# 문자열 내 마음대로 정렬하기
+# 나누어 떨어지는 숫자 배열
 
-def solution(strings, n):
-    for i in range(len(strings) - 1, 0, -1):
-        for j in range(i):
-            if (strings[j][n] > strings[j + 1][n]):
-                strings[j], strings[j + 1] = strings[j + 1], strings[j]
+def solution(arr, divisor):
+    arr.sort()
+    result = []
+    for i in range(len(arr)):
+        if (arr[i] % divisor == 0):
+            result.append(arr[i])
+    if (len(result) == 0):
+        result.append(-1)
 
-            elif (strings[j][n] == strings[j + 1][n]):
-                len_ = min(len(strings[j]), len(strings[j + 1]))
-                isBig = False
-                isChange = False
-                for k in range(len_):
-                    if (strings[j][k] > strings[j + 1][k]):
-                        strings[j], strings[j + 1] = strings[j + 1], strings[j]
-                        isChange = True
-                        break
-                    elif (strings[j][k] < strings[j + 1][k]):
-                        isBig = True
-                        break
-                if (len(strings[j]) > len(strings[j + 1]) and isBig == False and isChange == False):
-                    strings[j], strings[j + 1] = strings[j + 1], strings[j]
-
-    return strings
+    return result
