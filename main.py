1b82e38794bd0cc1e95da73cb94d63fbdeb9bfea
Izalpha <visualstudio530@naver.com>
2021-06-13 00:15:27 +0900

    프로그래머스 - 시저 암호

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-# 서울에서 김서방 찾기
+# 시저 암호
 
-def solution(seoul):
-    a = seoul.index("Kim")
-    answer = f"김서방은 {a}에 있다"
-    return answer
+def solution(s, n):
+    result = [i for i in s]
+
+    for i in range(len(s)):
+        for j in range(26):
+            if (ord(s[i]) == ord('a') + j):
+                if (chr(ord('a') + j + n) > 'z'):
+                    result[i] = chr(ord('a') + j + n - 26)
+                else:
+                    result[i] = chr(ord('a') + j + n)
+
+            elif (ord(s[i]) == ord('A') + j):
+                if (chr(ord('A') + j + n) > 'Z'):
+                    result[i] = chr(ord('A') + j + n - 26)
+                else:
+                    result[i] = chr(ord('A') + j + n)
+
+    ans = ""
+    for i in range(len(result)):
+        ans += result[i]
+
+    return ans
