e1c978f2df0895945b148815b6253860ee740df6
Izalpha <visualstudio530@naver.com>
2021-06-13 04:11:09 +0900

    프로그래머스 - 폰켓몬

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
-def solution(lottos, win_nums):
-    count = 0
-    zero = 0
-    for i in range(len(lottos)):
-        if (lottos[i] == 0):
-            zero += 1
+def solution(nums):
+    get = []
+    for i in range(len(nums)):
+        if (len(get) == len(nums) / 2):
+            return len(get)
 
-    for i in range(len(lottos)):
-        for j in range(len(lottos)):
-            if (lottos[i] == win_nums[j]):
-                count += 1
+        if (nums[i] not in get):
+            get.append(nums[i])
 
-    good = count + zero
-    bad = count
-    result = []
-
-    if (good == 6):
-        result.append(1)
-    elif (good == 5):
-        result.append(2)
-    elif (good == 4):
-        result.append(3)
-    elif (good == 3):
-        result.append(4)
-    elif (good == 2):
-        result.append(5)
-    else:
-        result.append(6)
-
-    if (bad == 6):
-        result.append(1)
-    elif (bad == 5):
-        result.append(2)
-    elif (bad == 4):
-        result.append(3)
-    elif (bad == 3):
-        result.append(4)
-    elif (bad == 2):
-        result.append(5)
-    else:
-        result.append(6)
-
-    return result
+    return len(get)
