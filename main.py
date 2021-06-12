e8db982aa8adc15e2d0257cd72b5d83305417f78
Izalpha <visualstudio530@naver.com>
2021-06-13 00:11:05 +0900

    프로그래머스 - 다트 게임 (무지성 코드)

--- main.py
+++ main.py
@@ -1,12 +1,113 @@
-def solution(arr):
+# 다트 게임
+def solution(dartResult):
     i = 0
-    arr2 = []
-    for i in range(1, len(arr)):
-        if (arr[i] == arr[i - 1] and arr[i] != -1):
-            arr[i - 1] = -1
+    dart = []
+    while (i <= len(dartResult) - 2):
+        result = ""
+        if (len(dartResult) - 1 >= i + 2):
+            result += dartResult[i]
+            result += dartResult[i + 1]
+            if (dartResult[i + 2] == "*"):
+                result += dartResult[i + 2]
+                i += 1
+            elif (dartResult[i + 2] == "#"):
+                result += dartResult[i + 2]
+                i += 1
+            elif (dartResult[i + 2] == "S" or dartResult[i + 2] == "D" or dartResult[i + 2] == "T"):
+                result += dartResult[i + 2]
+                i += 1
+            i += 2
+        else:
+            result += dartResult[i]
+            result += dartResult[i + 1]
+            i += 2
 
-    for i in range(len(arr)):
-        if (arr[i] != -1):
-            arr2.append(arr[i])
+        dart.append(result)
 
-    return arr2
+    score = [0, 0, 0]
+    for i in range(len(dart)):
+        if (len(dart[i]) == 3 and i == 0):
+            if (dart[i][1] == "S"):
+                score[i] += int(dart[i][0])
+            elif (dart[i][1] == "D"):
+                score[i] += int(dart[i][0]) * int(dart[i][0])
+            elif (dart[i][1] == "T"):
+                score[i] += int(dart[i][0]) * int(dart[i][0]) * int(dart[i][0])
+            else:
+                score[i] += int(dart[i][0]) * 10 + int(dart[i][1])
+
+            if (dart[i][2] == "*"):
+                score[i] *= 2
+            elif (dart[i][2] == "#"):
+                score[i] *= -1
+            elif (dart[i][2] == "S"):
+                score[i] = score[i]
+            elif (dart[i][2] == "D"):
+                score[i] = score[i] * score[i]
+            elif (dart[i][2] == "T"):
+                score[i] = score[i] * score[i] * score[i]
+
+        elif (len(dart[i]) == 3):
+            if (dart[i][1] == "S"):
+                score[i] += int(dart[i][0])
+            elif (dart[i][1] == "D"):
+                score[i] += int(dart[i][0]) * int(dart[i][0])
+            elif (dart[i][1] == "T"):
+                score[i] += int(dart[i][0]) * int(dart[i][0]) * int(dart[i][0])
+            else:
+                score[i] += int(dart[i][0]) * 10 + int(dart[i][1])
+
+            if (dart[i][2] == "*"):
+                score[i] *= 2
+                score[i - 1] *= 2
+            elif (dart[i][2] == "#"):
+                score[i] *= -1
+            elif (dart[i][2] == "S"):
+                score[i] = score[i]
+            elif (dart[i][2] == "D"):
+                score[i] = score[i] * score[i]
+            elif (dart[i][2] == "T"):
+                score[i] = score[i] * score[i] * score[i]
+
+        elif (len(dart[i]) == 2):
+            if (dart[i][1] == "S"):
+                score[i] += int(dart[i][0])
+            elif (dart[i][1] == "D"):
+                score[i] += int(dart[i][0]) * int(dart[i][0])
+            else:
+                score[i] += int(dart[i][0]) * int(dart[i][0]) * int(dart[i][0])
+
+        elif (len(dart[i] == 4 and i == 0)):
+            if (dart[i][2] == "S"):
+                score[i] += dart[i][0] * 10 + dart[i][1]
+            elif (dart[i][2] == "D"):
+                score[i] += (dart[i][0] * 10 + dart[i][1]) * (dart[i][0] * 10 + dart[i][1])
+            elif (dart[i][3] == "T"):
+                score[i] += (dart[i][0] * 10 + dart[i][1]) * (dart[i][0] * 10 + dart[i][1]) * (
+                            dart[i][0] * 10 + dart[i][1])
+
+            if (dart[i][3] == "*"):
+                score[i] *= 2
+            elif (dart[i][3] == "#"):
+                score[i] *= -1
+
+        elif (len(dart[i] == 4)):
+            if (dart[i][2] == "S"):
+                score[i] += dart[i][0] * 10 + dart[i][1]
+            elif (dart[i][2] == "D"):
+                score[i] += (dart[i][0] * 10 + dart[i][1]) * (dart[i][0] * 10 + dart[i][1])
+            elif (dart[i][3] == "T"):
+                score[i] += (dart[i][0] * 10 + dart[i][1]) * (dart[i][0] * 10 + dart[i][1]) * (
+                            dart[i][0] * 10 + dart[i][1])
+
+            if (dart[i][3] == "*"):
+                score[i] *= 2
+                score[i - 1] *= 2
+            elif (dart[i][3] == "#"):
+                score[i] *= -1
+
+        ans = 0
+    for i in range(len(score)):
+        ans += score[i]
+
+    return ans
