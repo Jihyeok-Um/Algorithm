8bbaaf5883897b2b0e9c938e41c56f8579b1bf25
Izalpha <visualstudio530@naver.com>
2021-06-14 01:06:40 +0900

    프로그래머스 - 내적

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-# 두 개 뽑아서 더하기
-
-def solution(nums):
-    get = []
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if ((nums[i] + nums[j]) not in get):
-                get.append(nums[i] + nums[j])
-
-    get.sort()
-    return get
+def solution(a, b):
+    result = 0
+    for i in range(len(a)):
+        result += a[i]*b[i]
+    return result
