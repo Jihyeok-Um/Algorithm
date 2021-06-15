3d98103eccde2f9f00716bdfd0ec25ab3870247e
Izalpha <visualstudio530@naver.com>
2021-06-15 20:17:36 +0900

    프로그래머스 - 땅따먹기

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-def solution(s):
-    srr = ""
-    arr = []
-    for i in range(len(s)):
-        if (s[i] != " "):
-            srr += s[i]
-        elif (s[i] == " "):
-            arr.append(srr)
-            srr = ""
-    arr.append(srr)
-    for i in range(len(arr)):
-        arr[i] = int(arr[i])
+def solution(land):
+    s = [[0 for i in range(4)] for i in range(len(land))]
+    for i in range(len(land[0])):
+        s[0][i] = land[0][i]
 
-    ans = ""
-    ans += str(min(arr))
-    ans += " "
-    ans += str(max(arr))
-    return ans
+    for i in range(1, len(land)):
+        for j in range(len(land[0])):
+            for k in range(len(land[0])):
+                if (j != k):
+                    if (s[i][j] < s[i - 1][k]):
+                        s[i][j] = s[i - 1][k]
+            s[i][j] += land[i][j]
+
+    return max(s[len(land) - 1])
