8fa53968830328a56ef1720fd23a850ac88c273a
Izalpha <visualstudio530@naver.com>
2021-06-15 23:56:55 +0900

    프로그래머스 - 최솟값 만들기

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from math import gcd
+def solution(A, B):
+    ans = 0
+    A.sort()
+    B.sort(reverse=True)
+    for i in range(len(A)):
+        ans += A[i] * B[i]
 
-def solution(arr):
-    ans = lcm(arr[0], arr[1])
-    for i in range(2, len(arr)):
-        ans = lcm(ans, arr[i])
     return ans
-
-
-def lcm(x, y):
-    return x * y // gcd(x, y)
