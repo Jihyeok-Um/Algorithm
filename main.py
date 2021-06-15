1ef99dcd0992367c6d22438c05c3fee606291db7
Izalpha <visualstudio530@naver.com>
2021-06-15 21:30:14 +0900

    프로그래머스 - N개의 최소공배수

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-def solution(s):
-    count = 0
-    for i in range(len(s)):
-        if(s[i] == "("):
-            count += 1
-        else:
-            if (count > 0):
-                count -= 1
-            else:
-                return False
+from math import gcd
 
-    if (count == 0):
-        return True
-    else:
-        return False
+def solution(arr):
+    ans = lcm(arr[0], arr[1])
+    for i in range(2, len(arr)):
+        ans = lcm(ans, arr[i])
+    return ans
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
