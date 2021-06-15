9c6c9a48a8260136272bce8e87d89caee00ef5da
Izalpha <visualstudio530@naver.com>
2021-06-15 20:28:57 +0900

    프로그래머스 - 올바른 괄호

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-def solution(land):
-    s = [[0 for i in range(4)] for i in range(len(land))]
-    for i in range(len(land[0])):
-        s[0][i] = land[0][i]
+def solution(s):
+    count = 0
+    for i in range(len(s)):
+        if(s[i] == "("):
+            count += 1
+        else:
+            if (count > 0):
+                count -= 1
+            else:
+                return False
 
-    for i in range(1, len(land)):
-        for j in range(len(land[0])):
-            for k in range(len(land[0])):
-                if (j != k):
-                    if (s[i][j] < s[i - 1][k]):
-                        s[i][j] = s[i - 1][k]
-            s[i][j] += land[i][j]
-
-    return max(s[len(land) - 1])
+    if (count == 0):
+        return True
+    else:
+        return False
