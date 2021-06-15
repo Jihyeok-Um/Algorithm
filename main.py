9418f0d577b625d896b3280c6a762b0ac0cf4344
Izalpha <visualstudio530@naver.com>
2021-06-16 00:18:03 +0900

    프로그래머스 - 다음 큰 숫자

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 def solution(n):
-    i = 0
-    ans = count = 0
-    re = 1
-    while (i <= n):
-        i += 1
-        ans += i
-        if (ans == n):
-            count += 1
-            i = re
-            re += 1
-        elif (ans > n):
-            ans = 0
-            i = re
-            re += 1
+    one = getOne(n)
+    while(True):
+        n += 1
+        if(one == getOne(n)):
+            return n
 
-    return count
+def getOne(n):
+    arr = []
+    while(n > 0):
+        arr.append(n % 2)
+        n = n // 2
+    return arr.count(1)
