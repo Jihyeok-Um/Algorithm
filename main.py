39287cc97a238999ddd87a479db78c5aa3d8b898
Izalpha <visualstudio530@naver.com>
2021-06-17 01:11:14 +0900

    프로그래머스 - 이진탐색

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-from itertools import permutations
+def solution(L, x):
+    start = 0
+    last = len(L) - 1
 
-prime = [False for i in range(10000001)]
-
-
-def isPrime():
-    prime[0] = prime[1] = True
-    for i in range(2, 10000000):
-        if (prime[i] == False):
-            j = i * i
-            while (j <= 10000000):
-                prime[j] = True
-                j += i
-
-
-def solution(numbers):
-    isPrime()
-    count = 0
-    primeStore = []
-    for i in range(len(numbers)):
-        num = list(permutations(numbers, i + 1))
-        for j in range(len(num)):
-            temp = ""
-            for k in range(len(num[j])):
-                temp += num[j][k]
-            if (prime[int(temp)] == False and int(temp) not in primeStore):
-                primeStore.append(int(temp))
-                count += 1
-
-    return count
-
+    while (start <= last):
+        mid = (start + last) // 2
+        if (L[mid] > x):
+            last = mid - 1
+        elif (L[mid] < x):
+            start = mid + 1
+        elif (L[mid] == x):
+            return mid
+    return -1
