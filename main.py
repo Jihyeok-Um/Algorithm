2ad6fb164a936c554dcd65b2fac83b2d02206236
Izalpha <visualstudio530@naver.com>
2021-06-17 23:16:27 +0900

    프로그래머스 - 이중연결리스트 (popAfter, popBefore, popAt)

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-def insertBefore(self, next, newNode):
-    if next is self.head:
-        return False
-    prev = next.prev
-    newNode.next = next
-    newNode.prev = prev
-    next.prev = newNode
-    prev.next = newNode
-    self.nodeCount += 1
+def popAfter(self, prev):
+    if self.head == self.tail:
+        return None
 
-    return True
+    next = prev.next.next
+    curr = prev.next
+    next.prev = prev
+    prev.next = next
+    self.nodeCount -= 1
+    return curr.data
+
+
+def popBefore(self, next):
+    if self.head == self.tail:
+        return None
+
+    prev = next.prev.prev
+    curr = next.prev
+    next.prev = prev
+    prev.next = next
+    self.nodeCount -= 1
+    return curr.data
+
+
+def popAt(self, pos):
+    if pos < 0 or pos > self.nodeCount:
+        raise IndexError
+    if pos > self.nodeCount // 2:
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+    else:
+        next = self.getAt(pos + 1)
+        return self.popBefore(next)
