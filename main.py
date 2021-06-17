07a89fec84beac3fc7d3c147daea0093526cfc14
Izalpha <visualstudio530@naver.com>
2021-06-18 01:24:00 +0900

    프로그래머스 - 스킬트리

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-def popAfter(self, prev):
-    if self.head == self.tail:
-        return None
+def solution(skill, skill_trees):
+    count = 0
+    for i in range(len(skill_trees)):
+        temp = []
+        for j in range(len(skill)):
+            ans = skill_trees[i].find(skill[j])
+            if (ans == -1):
+                temp.append(27)
+            else:
+                temp.append(ans)
 
-    next = prev.next.next
-    curr = prev.next
-    next.prev = prev
-    prev.next = next
-    self.nodeCount -= 1
-    return curr.data
-
-
-def popBefore(self, next):
-    if self.head == self.tail:
-        return None
-
-    prev = next.prev.prev
-    curr = next.prev
-    next.prev = prev
-    prev.next = next
-    self.nodeCount -= 1
-    return curr.data
-
-
-def popAt(self, pos):
-    if pos < 0 or pos > self.nodeCount:
-        raise IndexError
-    if pos > self.nodeCount // 2:
-        prev = self.getAt(pos - 1)
-        return self.popAfter(prev)
-    else:
-        next = self.getAt(pos + 1)
-        return self.popBefore(next)
+        print(temp)
+        con = False
+        for j in range(len(temp) - 1):
+            if (temp[j] > temp[j + 1]):
+                con = True
+                break
+        if (con == True):
+            continue
+        count += 1
+    return count
