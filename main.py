9b275985121c256a3d12327e47c061ee6ef995da
Izalpha <visualstudio530@naver.com>
2021-06-18 04:55:59 +0900

    프로그래머스 - 스택(중위표현식 -> 후위표현식)

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-from collections import deque
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
 
 
-def solution(dirs):
-    check = [[[] for i in range(11)] for i in range(11)]
-    dy = [-1, 0, 1, 0]
-    dx = [0, 1, 0, -1]
-    char = [5, 5]
-    count = 0
-    d = deque()
-    d.append(char)
-    for i in range(len(dirs)):
-        temp = d.popleft()
-        if (dirs[i] == "U"):
-            if (char[0] + dy[0] >= 0):
-                char = [char[0] + dy[0], char[1]]
-        elif (dirs[i] == "L"):
-            if (char[1] + dx[3] >= 0):
-                char = [char[0], char[1] + dx[3]]
-        elif (dirs[i] == "D"):
-            if (char[0] + dy[2] <= 10):
-                char = [char[0] + dy[2], char[1]]
+prec = {
+    '*': 3, '/': 3,
+    '+': 2, '-': 2,
+    '(': 1
+}
+
+
+def solution(S):
+    opStack = ArrayStack()
+    ans = ""
+    for i in range(len(S)):
+        if (S[i] in prec):
+            if (S[i] != '('):
+                while (not opStack.isEmpty() and prec[opStack.peek()] >= prec[S[i]]):
+                    ans += opStack.pop()
+            opStack.push(S[i])
+        elif (S[i] == ')'):
+            while (prec[opStack.peek()] != 1):
+                ans += opStack.pop()
+            opStack.pop()
         else:
-            if (char[1] + dx[1] <= 10):
-                char = [char[0], char[1] + dx[1]]
+            ans += S[i]
 
-        if (dirs[i] not in check[char[0]][char[1]] and temp != char):
-            check[char[0]][char[1]].append(dirs[i])
-            if (dirs[i] == "U"):
-                check[temp[0]][temp[1]].append("D")
-            elif (dirs[i] == "D"):
-                check[temp[0]][temp[1]].append("U")
-            elif (dirs[i] == "R"):
-                check[temp[0]][temp[1]].append("L")
-            elif (dirs[i] == "L"):
-                check[temp[0]][temp[1]].append("R")
-            count += 1
-        d.append(char)
+    while (not opStack.isEmpty()):
+        ans += opStack.pop()
 
-    print(check)
-    return count
+    return ans
