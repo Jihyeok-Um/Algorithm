f84762cb478a3aea4604bf02d85d9bcb3c8c55e5
Izalpha <visualstudio530@naver.com>
2021-06-19 22:06:52 +0900

    프로그래머스 - 주식가격(코드분석)

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from collections import deque
-
-
-def solution(priorities, location):
-    q = deque()
-    for i in priorities:
-        q.append(i)
-
-    count = 0
-    location += 1
-    while (q):
-        temp = q.popleft()
-        if (location > 0):
-            location -= 1
-
-        if (len(q) >= 1 and temp >= max(q)):
-            count += 1
-            if (location == 0):
-                return count
-        elif (len(q) == 0):
-            count += 1
-            if (location == 0):
-                return count
-        else:
-            q.append(temp)
-            if (location == 0):
-                location = len(q)
+def solution(prices):
+    answer = []
+    for i in range(len(prices) - 1):
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            cnt += 1
+            if prices[i] > prices[j]:
+                break
+        answer.append(cnt)
+    answer.append(0)
+    return answer
