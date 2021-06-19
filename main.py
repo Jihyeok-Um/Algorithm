616a0d2073d35a2778d1413180e7006214644ef3
Izalpha <visualstudio530@naver.com>
2021-06-20 08:04:06 +0900

    프로그래머스 - 다리를 지나는 트럭

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-def solution(prices):
-    answer = []
-    for i in range(len(prices) - 1):
-        cnt = 0
-        for j in range(i+1, len(prices)):
-            cnt += 1
-            if prices[i] > prices[j]:
-                break
-        answer.append(cnt)
-    answer.append(0)
-    return answer
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    q2 = deque()
+    q1 = deque(0 for i in range(bridge_length))
+    q = deque(truck_weights)
+    time = zeroCount = numTruck = 0
+
+    while (len(q2) < len(truck_weights)):
+        if (len(q) > 0 and sum(q1) + q[0] <= weight and numTruck + 1 <= bridge_length):
+            t = q.popleft()
+            q1.append(t)
+            numTruck += 1
+            zeroCount = 0
+        else:
+            if (len(q1) < bridge_length):
+                q1.append(0)
+
+        t2 = q1.popleft()
+        if (t2 != 0):
+            q2.append(t2)
+            numTruck -= 1
+            if (len(q) > 0 and sum(q1) + q[0] <= weight and numTruck + 1 <= bridge_length):
+                t = q.popleft()
+                q1.append(t)
+                numTruck += 1
+
+        time += 1
+    return time
