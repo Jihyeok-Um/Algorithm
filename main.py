2da2df7fcb82624edc5bf51890c502f6e440c2cc
Izalpha <visualstudio530@naver.com>
2021-06-20 10:22:30 +0900

    프로그래머스 - 완주하지 못한 선수

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from collections import deque
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    result = ""
+    for i in range(len(completion)):
+        if (completion[i] != participant[i]):
+            result = participant[i]
+            break
 
-def solution(bridge_length, weight, truck_weights):
-    q2 = deque()
-    q1 = deque(0 for i in range(bridge_length))
-    q = deque(truck_weights)
-    time = zeroCount = numTruck = 0
-
-    while (len(q2) < len(truck_weights)):
-        if (len(q) > 0 and sum(q1) + q[0] <= weight and numTruck + 1 <= bridge_length):
-            t = q.popleft()
-            q1.append(t)
-            numTruck += 1
-            zeroCount = 0
-        else:
-            if (len(q1) < bridge_length):
-                q1.append(0)
-
-        t2 = q1.popleft()
-        if (t2 != 0):
-            q2.append(t2)
-            numTruck -= 1
-            if (len(q) > 0 and sum(q1) + q[0] <= weight and numTruck + 1 <= bridge_length):
-                t = q.popleft()
-                q1.append(t)
-                numTruck += 1
-
-        time += 1
-    return time
+    if (result == ""):
+        return participant[len(participant) - 1]
+    else:
+        return result
