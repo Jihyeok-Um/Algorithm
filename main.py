b213bbd9f9aa842af87663a1148420283c800207
Izalpha <visualstudio530@naver.com>
2021-06-22 14:05:17 +0900

    프로그래머스 - 점프와 순간이동

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-def solution(s):
-    minlen = 1000
-    for i in range(len(s) // 2):
-        j = 0
-        count = 1
-        arr = ""
-        spl = []
-        while (j < len(s)):
-            spl.append(s[j:j + (i + 1)])
-            j += i + 1
+def solution(n):
+    count = 0
+    while (n != 1):
+        if (n % 2 == 0):
+            n = n // 2
+        elif (n % 2 == 1):
+            n = n - 1
+            count += 1
 
-        for j in range(len(spl) - 1):
-            if (spl[j] == spl[j + 1]):
-                count += 1
-            elif (spl[j] != spl[j + 1]):
-                if (count != 1):
-                    arr += str(count)
-                arr += spl[j]
-                count = 1
-
-        if (count != 1):
-            arr += str(count)
-        arr += spl[j + 1]
-
-        minlen = min(len(arr), minlen)
-
-    if (len(s) == 1):
-        return 1
-    return minlen
+    return count + 1
