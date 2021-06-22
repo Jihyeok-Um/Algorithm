56ea7329b75874f587526b966e6cb5904e15862f
Izalpha <visualstudio530@naver.com>
2021-06-22 09:17:25 +0900

    프로그래머스 - 예상대진표

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import heapq
-
-def solution(scoville, K):
-    heap = []
-    for i in scoville:
-        heapq.heappush(heap, i)
-
+def solution(n,a,b):
     count = 0
-    while (heap[0] < K and len(heap) >= 2):
-        a = heapq.heappop(heap)
-        b = heapq.heappop(heap)
-        result = a + b * 2
-        heapq.heappush(heap, result)
+    while(a != b):
+        a = (a+1) // 2
+        b = (b+1) // 2
         count += 1
-
-    if (heap[0] < K):
-        return -1
-    return count 
+    return count
