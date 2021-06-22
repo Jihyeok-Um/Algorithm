af362ef644ac8909b2b03f5129b934a6d1b69cb7
Izalpha <visualstudio530@naver.com>
2021-06-22 14:04:39 +0900

    프로그래머스 - 문자열 압축

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-def solution(n, words):
-    num = 2
-    count = 1
-    a = {}
-    a[words[0]] = 1
-    for i in range(1, len(words)):
-        if (words[i] not in a):
-            a[words[i]] = 1
-        else:
-            a[words[i]] = a[words[i]] + 1
+def solution(s):
+    minlen = 1000
+    for i in range(len(s) // 2):
+        j = 0
+        count = 1
+        arr = ""
+        spl = []
+        while (j < len(s)):
+            spl.append(s[j:j + (i + 1)])
+            j += i + 1
 
-        if (a[words[i]] > 1 or words[i - 1][len(words[i - 1]) - 1] != words[i][0]):
-            return [num, count]
+        for j in range(len(spl) - 1):
+            if (spl[j] == spl[j + 1]):
+                count += 1
+            elif (spl[j] != spl[j + 1]):
+                if (count != 1):
+                    arr += str(count)
+                arr += spl[j]
+                count = 1
 
-        if (num == n):
-            num = 1
-            count += 1
-        else:
-            num += 1
+        if (count != 1):
+            arr += str(count)
+        arr += spl[j + 1]
 
-    return [0, 0]
+        minlen = min(len(arr), minlen)
+
+    if (len(s) == 1):
+        return 1
+    return minlen
