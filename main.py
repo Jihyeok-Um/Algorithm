056a9eb530d8e7f70dc4db85e7aae7a96a104b8d
Izalpha <visualstudio530@naver.com>
2021-06-22 12:10:19 +0900

    프로그래머스 - 영어 끝말잇기

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def solution(n,a,b):
-    count = 0
-    while(a != b):
-        a = (a+1) // 2
-        b = (b+1) // 2
-        count += 1
-    return count
+def solution(n, words):
+    num = 2
+    count = 1
+    a = {}
+    a[words[0]] = 1
+    for i in range(1, len(words)):
+        if (words[i] not in a):
+            a[words[i]] = 1
+        else:
+            a[words[i]] = a[words[i]] + 1
+
+        if (a[words[i]] > 1 or words[i - 1][len(words[i - 1]) - 1] != words[i][0]):
+            return [num, count]
+
+        if (num == n):
+            num = 1
+            count += 1
+        else:
+            num += 1
+
+    return [0, 0]
