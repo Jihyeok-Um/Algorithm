4552c61189f65c38b08fe36ab4cb920e48b1a633
Izalpha <visualstudio530@naver.com>
2021-06-26 17:12:46 +0900

    프로그래머스 - 게임 맵 최단거리

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-def solution(N, stages):
-    answer = {}
-    for i in range(1, N + 1):
-        clearCount = count = 0
-        for j in range(len(stages)):
-            if (i < stages[j]):
-                clearCount += 1
-                count += 1
-            elif (i == stages[j]):
-                count += 1
-        if (count == 0):
-            answer[i] = 0.0
-        else:
-            answer[i] = (count - clearCount) / count
+from collections import deque
 
-    print(answer)
-    answer = sorted(answer, key=lambda x: answer[x], reverse=True)
-    print(answer)
-    return answer
+def solution(maps):
+    n = len(maps[0])
+    m = len(maps)
+
+    check = [[0 for i in range(n)] for i in range(m)]
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    q = deque()
+    q.append([0, 0])
+    check[0][0] = 1
+    while (q):
+        c = q.popleft()
+        for i in range(4):
+            x = c[1] + dx[i]
+            y = c[0] + dy[i]
+            if (x >= 0 and x < n and y >= 0 and y < m and maps[y][x] == 1 and check[y][x] == 0):
+                q.append([y, x])
+                check[y][x] = check[c[0]][c[1]] + 1
+
+    for i in range(len(maps)):
+        print(check[i])
+
+    if (check[m - 1][n - 1] != 0):
+        return check[m - 1][n - 1]
+    else:
+        return -1
