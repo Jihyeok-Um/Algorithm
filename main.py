c401db8efdf895ab2a37ef1587dd87d1917bbc5c
Izalpha <visualstudio530@naver.com>
2021-06-30 18:16:18 +0900

    프로그래머스 - 뉴스 클러스터링

--- main.py
+++ main.py
@@ -1,67 +1,45 @@
-def numOne(p):
-    if (len(p) == 0):
-        return True
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def numTwo(p):
-    u = ""
-    v = ""
-    inCount = outCount = 0
-    for i in p:
-        if (inCount != outCount or inCount == outCount == 0):
-            if (i == '('):
-                inCount += 1
-                u += i
+
+def solution(str1, str2):
+    no = deno = 0
+    arr1 = []
+    arr2 = []
+    str1 = str1.lower()
+    str2 = str2.lower()
+    for i in range(len(str1) - 1):
+        if (str1[i] in alpha and str1[i + 1] in alpha):
+            arr1.append(str1[i:i + 2])
+    for i in range(len(str2) - 1):
+        if (str2[i] in alpha and str2[i + 1] in alpha):
+            arr2.append(str2[i:i + 2])
+
+    if (len(arr1) == 0 and len(arr2) == 0):
+        return int(1 * 65536)
+    else:
+
+        val = []
+        arr1.sort()
+        arr2.sort()
+        for i in range(len(arr1)):
+            if (arr1[i] in arr2 and arr1[i] not in val):
+                no += min(arr1.count(arr1[i]), arr2.count(arr1[i]))
+                val.append(arr1[i])
+
+        dic = {}
+        for i in range(len(arr1)):
+            if (arr1[i] not in dic):
+                dic[arr1[i]] = arr1.count(arr1[i])
+
+        for i in range(len(arr2)):
+            if (arr2[i] not in dic):
+                dic[arr2[i]] = arr2.count(arr2[i])
             else:
-                outCount += 1
-                u += i
-        elif (inCount == outCount):
-            v += i
-    return u, v
+                dic[arr2[i]] = max(dic[arr2[i]], arr2.count(arr2[i]))
 
-def numThree(p):
-    stack = []
-    for i in p:
-        if (i == '('):
-            stack.append(i)
-        elif (i == ')' and len(stack) > 0 and stack[-1] == '('):
-            stack.pop()
-        else:
-            stack.append(i)
+        ans = list(dic.values())
+        for i in ans:
+            deno += i
 
-    if (len(stack) == 0):
-        return True
-
-def getNewU(u):
-    newU = ""
-    for i in range(len(u)):
-        if (i == 0 or i == len(u) - 1):
-            pass
-        else:
-            if (u[i] == '('):
-                newU += ')'
-            elif (u[i] == ')'):
-                newU += '('
-
-    return newU
-
-def sol(p):
-    if (numOne(p)):
-        return ""
-    u, v = map(str, numTwo(p))
-    if (numThree(u)):
-        return u + sol(v)
-    else:
-        return "(" + sol(v) + ")" + getNewU(u)
-
-
-def solution(p):
-    if (numOne(p)):
-        return ""
-    if (numThree(p)):
-        return p
-    else:
-        return sol(p)
-
-
-
-
+        return int((no / deno) * 65536)
