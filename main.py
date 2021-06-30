e18d21dc27b4d740fb314027e0dcfe2dc546b58a
Izalpha <visualstudio530@naver.com>
2021-06-30 22:04:12 +0900

    프로그래머스 - 괄호 회전하기

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-def solution(board):
-    for y in range(len(board) - 1):
-        for x in range(len(board[y]) - 1):
-            if (board[y + 1][x + 1] != 0):
-                board[y + 1][x + 1] = min(board[y][x], board[y + 1][x], board[y][x + 1]) + 1
+def isRight(s):
+    stack = []
+    for i in s:
+        if (i == '[' or i == '{' or i == '('):
+            stack.append(i)
+        elif ((i == ']' or i == '}' or i == ')') and len(stack) > 0):
+            if (stack[-1] == '[' and i == ']' or stack[-1] == '{' and i == '}' or stack[-1] == '(' and i == ')'):
+                stack.pop()
+        else:
+            stack.append(i)
 
+    if (len(stack) == 0):
+        return True
+    else:
+        return False
+
+
+def sRotate(s):
+    f = ""
+    newS = ""
+    for i in range(len(s)):
+        if (i == 0):
+            f += s[i]
+        else:
+            newS += s[i]
+
+    return newS + f
+
+
+def solution(s):
     ans = 0
-    for y in range(len(board)):
-        ans = max(ans, max(board[y]))
+    for i in range(len(s)):
+        if (isRight(s)):
+            ans += 1
+        s = sRotate(s)
 
-    return ans * ans
+    return ans
