c1c5a8ee978be53b5c44a365164a98968292567d
Izalpha <visualstudio530@naver.com>
2021-07-01 20:43:25 +0900

    프로그래머스 - 튜플

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-def solution(n, t, m, p):
-    arr = ["F", 0]
-    stack = []
-    i = count = 1
-    while (len(arr) < p + m * t):
-        i = count
-        count += 1
-        while (i != 0):
-            stack.append((i % n))
-            i = i // n
+def solution(s):
+    s = s[1:-1]
+    ans = []
+    i = 0
+    while (i < len(s)):
+        if (s[i] == '{'):
+            i += 1
+            temp = ""
+            small = []
+            while (s[i] != '}'):
+                if (s[i] != ','):
+                    temp += s[i]
+                else:
+                    small.append(int(temp))
+                    temp = ""
+                i += 1
+            small.append(int(temp))
+            ans.append(small)
+        i += 1
 
-        for k in range(len(stack)):
-            temp = stack.pop()
-            if (temp == 10):
-                temp = 'A'
-            elif (temp == 11):
-                temp = 'B'
-            elif (temp == 12):
-                temp = 'C'
-            elif (temp == 13):
-                temp = 'D'
-            elif (temp == 14):
-                temp = 'E'
-            elif (temp == 15):
-                temp = 'F'
-            arr.append(temp)
+    answer = []
+    ans.sort(key=len)
+    for i in range(len(ans)):
+        for j in range(len(ans[i])):
+            if (ans[i][j] not in answer):
+                answer.append(ans[i][j])
 
-    ans = ""
-    for i in range(t):
-        ans += str(arr[p + m * i])
-    return ans
+    return answer
 
