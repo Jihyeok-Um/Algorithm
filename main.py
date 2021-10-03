04cb6a1cdb70a9eedb062aae313f67211e8bdb76
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-03 22:40:38 +0900

    프로그래머스 - 숫자 문자열과 영단어

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-def solution(n):
-    matrix = [[0 for i in range(n)] for j in range(n)]
-    matrix[0][0] = 1
-    count = 1
-    x = y = where = 0
-    num = n
+import sys
 
-    while (n != 1):
-        if (count == n):
-            count = 0
-            n -= 1
-            where += 1
+sys.setrecursionlimit(100000)
 
-        if (where % 3 == 0):
-            matrix[y + 1][x] = matrix[y][x] + 1
-            y = y + 1
-            count += 1
+num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+three = ["one", "two", "six"]
+four = ["zero", "four", "five", "nine"]
+five = ["three", "seven", "eight"]
 
-        elif (where % 3 == 1):
-            matrix[y][x + 1] = matrix[y][x] + 1
-            x = x + 1
-            count += 1
 
-        elif (where % 3 == 2):
-            matrix[y - 1][x - 1] = matrix[y][x] + 1
-            y = y - 1
-            x = x - 1
-            count += 1
+def solution(s):
+    result = ""
+    for i in range(50):
+        if (s[:1] in num):
+            result += s[:1]
+            s = s[1:]
+        elif (s[:3] in three):
+            ss = s[:3]
+            if (ss == three[0]):
+                result += "1"
+            elif (ss == three[1]):
+                result += "2"
+            elif (ss == three[2]):
+                result += "6"
+            s = s[3:]
+        elif (s[:4] in four):
+            ss = s[:4]
+            if (ss == four[0]):
+                result += "0"
+            elif (ss == four[1]):
+                result += "4"
+            elif (ss == four[2]):
+                result += "5"
+            elif (ss == four[3]):
+                result += "9"
+            s = s[4:]
+        elif (s[:5] in five):
+            ss = s[:5]
+            if (ss == five[0]):
+                result += "3"
+            elif (ss == five[1]):
+                result += "7"
+            elif (ss == five[2]):
+                result += "8"
+            s = s[5:]
 
-    ans = []
-    for y in range(num):
-        for x in range(num):
-            if (matrix[y][x] != 0):
-                ans.append(matrix[y][x])
-
-    return ans
+    return int(result)
