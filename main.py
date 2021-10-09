7a8db1fa7fb6f65887559867df1f1d4504e23c7b
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-10 00:24:09 +0900

    프로그래머스 - 2주차_상호평가

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-def solution(numbers):
-    num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    sum = 0
-    for i in num:
-        if (i not in numbers):
-            sum += i
+def solution(scores):
+    self = [0 for i in range((len(scores)))]
+    isExcept = [False for i in range(len(scores))]
+    isBig = [0 for i in range(len(scores))]
+    isSmall = [0 for i in range(len(scores))]
+    sumScore = [0 for i in range(len(scores))]
+    result = ""
 
-    return sum
+    for i in range(len(scores)):
+        for j in range(len(scores)):
+            sumScore[i] += scores[j][i]
+            if (i == j):
+                self[i] = scores[j][i]
+
+    for i in range(len(scores)):
+        for j in range(len(scores)):
+            if (self[i] > scores[j][i]):
+                isBig[i] += 1
+            if (self[i] < scores[j][i]):
+                isSmall[i] += 1
+
+    for i in range(len(isExcept)):
+        if (isBig[i] == len(isExcept) - 1 or isSmall[i] == len(isExcept) - 1):
+            isExcept[i] = True
+
+    print(sumScore)
+
+    for i in range(len(sumScore)):
+        if (isExcept[i] == True):
+            sumScore[i] -= self[i]
+            sumScore[i] = int(sumScore[i] / (len(sumScore) - 1))
+        else:
+            sumScore[i] = int(sumScore[i] / len(sumScore))
+
+    print(isExcept)
+    print(sumScore)
+
+    for i in range(len(sumScore)):
+        if (sumScore[i] >= 90):
+            result += "A"
+        elif (sumScore[i] >= 80):
+            result += "B"
+        elif (sumScore[i] >= 70):
+            result += "C"
+        elif (sumScore[i] >= 50):
+            result += "D"
+        else:
+            result += "F"
+
+    return result
