742796dcd3fac37dc7079a127c641b2be1fd5062
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-10 01:06:25 +0900

    프로그래머스 - 복서 정렬하기

--- main.py
+++ main.py
@@ -1,50 +1,23 @@
-def solution(scores):
-    self = [0 for i in range((len(scores)))]
-    isExcept = [False for i in range(len(scores))]
-    isBig = [0 for i in range(len(scores))]
-    isSmall = [0 for i in range(len(scores))]
-    sumScore = [0 for i in range(len(scores))]
-    result = ""
+def solution(weights, head2head):
+    verse = [[0, 0, weights[i], i] for i in range(len(weights))]
+    n = [0 for i in range(len(weights))]
+    for i in range(len(weights)):
+        for j in range(len(weights)):
+            if (head2head[i][j] == "W"):
+                verse[i][0] += 1
+                if (weights[j] > weights[i]):
+                    verse[i][1] += 1
+            elif (head2head[i][j] == "N"):
+                n[i] += 1
+    for i in range(len(verse)):
+        print(verse[i][0], len(verse) - n[i])
+        if (len(verse) - n[i] != 0):
+            verse[i][0] = float(verse[i][0]) / float(len(verse) - n[i])
+        print(verse[i][0])
 
-    for i in range(len(scores)):
-        for j in range(len(scores)):
-            sumScore[i] += scores[j][i]
-            if (i == j):
-                self[i] = scores[j][i]
-
-    for i in range(len(scores)):
-        for j in range(len(scores)):
-            if (self[i] > scores[j][i]):
-                isBig[i] += 1
-            if (self[i] < scores[j][i]):
-                isSmall[i] += 1
-
-    for i in range(len(isExcept)):
-        if (isBig[i] == len(isExcept) - 1 or isSmall[i] == len(isExcept) - 1):
-            isExcept[i] = True
-
-    print(sumScore)
-
-    for i in range(len(sumScore)):
-        if (isExcept[i] == True):
-            sumScore[i] -= self[i]
-            sumScore[i] = int(sumScore[i] / (len(sumScore) - 1))
-        else:
-            sumScore[i] = int(sumScore[i] / len(sumScore))
-
-    print(isExcept)
-    print(sumScore)
-
-    for i in range(len(sumScore)):
-        if (sumScore[i] >= 90):
-            result += "A"
-        elif (sumScore[i] >= 80):
-            result += "B"
-        elif (sumScore[i] >= 70):
-            result += "C"
-        elif (sumScore[i] >= 50):
-            result += "D"
-        else:
-            result += "F"
-
-    return result
+    result = sorted(verse, key=lambda x: (-x[0], -x[1], -x[2], x[3]))
+    print(result)
+    ans = [1 for i in range(len(weights))]
+    for i in range(len(verse)):
+        ans[i] += result[i][3]
+    return ans
