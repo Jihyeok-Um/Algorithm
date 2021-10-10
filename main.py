7ea1126f95af77c583860c97289a427bc2753f72
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-10 18:27:40 +0900

    프로그래머스 - 8주차_최소직사각형

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-def solution(weights, head2head):
-    verse = [[0, 0, weights[i], i] for i in range(len(weights))]
-    n = [0 for i in range(len(weights))]
-    for i in range(len(weights)):
-        for j in range(len(weights)):
-            if (head2head[i][j] == "W"):
-                verse[i][0] += 1
-                if (weights[j] > weights[i]):
-                    verse[i][1] += 1
-            elif (head2head[i][j] == "N"):
-                n[i] += 1
-    for i in range(len(verse)):
-        print(verse[i][0], len(verse) - n[i])
-        if (len(verse) - n[i] != 0):
-            verse[i][0] = float(verse[i][0]) / float(len(verse) - n[i])
-        print(verse[i][0])
+def solution(sizes):
+    garo = [0 for i in range(len(sizes))]
+    sero = [0 for i in range(len(sizes))]
+    for i in range(len(sizes)):
+        if (sizes[i][0] < sizes[i][1]):
+            sizes[i][0], sizes[i][1] = sizes[i][1], sizes[i][0]
+        garo[i] = sizes[i][0]
+        sero[i] = sizes[i][1]
 
-    result = sorted(verse, key=lambda x: (-x[0], -x[1], -x[2], x[3]))
-    print(result)
-    ans = [1 for i in range(len(weights))]
-    for i in range(len(verse)):
-        ans[i] += result[i][3]
-    return ans
+    result = max(garo) * max(sero)
+    return result
