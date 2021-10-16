5a85b653e2d6fd3ffe6595b06af65a0d3a9e0958
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-17 08:31:28 +0900

    백준 - 15662

--- main.py
+++ main.py
@@ -1,181 +1,41 @@
-s,n = map(int,input().split())
-n = str(n)
-num = [["" for i in range(2*s+3)] for i in range(10)]
+import sys
+from collections import deque
+t = int(input())
+cog = [0]
+turn = []
+for i in range(t):
+    cog.append(deque(sys.stdin.readline().rstrip()))
+k = int(input())
+for i in range(k):
+    turn.append(list(map(int, input().split())))
 
+def lowCog(cogNum, turn):
+    if (cog[cogNum-1][2] != cog[cogNum][6]):
+        chain[cogNum-1] = turn*-1
+        if (cogNum > 2):
+            lowCog(cogNum-1, chain[cogNum-1])
+    else:
+        return
+def highCog(cogNum, turn):
+    if (cog[cogNum][2] != cog[cogNum+1][6]):
+        chain[cogNum+1] = turn*-1
+        if(cogNum < len(cog)-2):
+            highCog(cogNum+1, chain[cogNum+1])
+    else:
+        return
 
-def makeOne(s):
-    for i in range(2*s+3):
-        if (i == 0 or i == s+1 or i == 2*s+2):
-            for j in range(s+2):
-                num[1][i] += " "
-        else:
-            for j in range(s+1):
-                num[1][i] += " "
-            num[1][i] += "|"
+for i in range(k):
+    chain = [0 for i in range(len(cog))]
+    chain[turn[i][0]] = turn[i][1]
+    if (turn[i][0] != 1):
+        lowCog(turn[i][0], turn[i][1])
+    if (turn[i][0] != len(cog)-1):
+        highCog(turn[i][0], turn[i][1])
+    for j in range(1, len(cog)):
+        cog[j].rotate(chain[j])
 
-def makeTwo(s):
-    for i in range(2*s+3):
-        if (i == 0 or i == s+1 or i == 2*s+2):
-            for j in range(s+2):
-                if (j == 0 or j == s+1):
-                    num[2][i] += " "
-                else:
-                    num[2][i] += "-"
-        elif (i > 0 and i <= s):
-            for j in range(s+1):
-                num[2][i] += " "
-            num[2][i] += "|"
-        else:
-            num[2][i] += "|"
-            for j in range(s+1):
-                num[2][i] += " "
-
-def makeThree(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == s + 1 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[3][i] += " "
-                else:
-                    num[3][i] += "-"
-        else:
-            for j in range(s + 1):
-                num[3][i] += " "
-            num[3][i] += "|"
-
-def makeFour(s):
-    for i in range(2*s+3):
-        if (i == 0 or i == 2*s+2):
-            for j in range(s+2):
-                num[4][i] += " "
-        elif (i == s+1):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[4][i] += " "
-                else:
-                    num[4][i] += "-"
-        elif (i > 0 and i <= s):
-            num[4][i] += "|"
-            for j in range(s):
-                num[4][i] += " "
-            num[4][i] += "|"
-        else:
-            for j in range(s+1):
-                num[4][i] += " "
-            num[4][i] += "|"
-
-def makeFive(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == s + 1 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[5][i] += " "
-                else:
-                    num[5][i] += "-"
-        elif (i > s+1 and i < 2*s+2):
-            for j in range(s + 1):
-                num[5][i] += " "
-            num[5][i] += "|"
-        else:
-            num[5][i] += "|"
-            for j in range(s + 1):
-                num[5][i] += " "
-
-def makeSix(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == s + 1 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[6][i] += " "
-                else:
-                    num[6][i] += "-"
-        elif (i > s+1 and i < 2*s+2):
-            num[6][i] += "|"
-            for j in range(s):
-                num[6][i] += " "
-            num[6][i] += "|"
-        else:
-            num[6][i] += "|"
-            for j in range(s + 1):
-                num[6][i] += " "
-
-def makeSeven(s):
-    for i in range(2 * s + 3):
-        if (i == 0):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[7][i] += " "
-                else:
-                    num[7][i] += "-"
-        elif(i == s + 1 or i == 2 * s + 2):
-            for j in range(s+2):
-                num[7][i] += " "
-        else:
-            for j in range(s+1):
-                num[7][i] += " "
-            num[7][i] += "|"
-
-def makeEight(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == s + 1 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[8][i] += " "
-                else:
-                    num[8][i] += "-"
-        else:
-            num[8][i] += "|"
-            for j in range(s):
-                num[8][i] += " "
-            num[8][i] += "|"
-
-def makeNine(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == s + 1 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[9][i] += " "
-                else:
-                    num[9][i] += "-"
-        elif(i > 0 and i <= s):
-            num[9][i] += "|"
-            for j in range(s):
-                num[9][i] += " "
-            num[9][i] += "|"
-        else:
-            for j in range(s+1):
-                num[9][i] += " "
-            num[9][i] += "|"
-
-def makeZero(s):
-    for i in range(2 * s + 3):
-        if (i == 0 or i == 2 * s + 2):
-            for j in range(s + 2):
-                if (j == 0 or j == s + 1):
-                    num[0][i] += " "
-                else:
-                    num[0][i] += "-"
-        elif (i == s+1):
-            for j in range(s+2):
-                num[0][i] += " "
-        else:
-            num[0][i] += "|"
-            for j in range(s):
-                num[0][i] += " "
-            num[0][i] += "|"
-
-makeOne(s)
-makeTwo(s)
-makeThree(s)
-makeFour(s)
-makeFive(s)
-makeSix(s)
-makeSeven(s)
-makeEight(s)
-makeNine(s)
-makeZero(s)
-
-for i in range(len(num[0])):
-    for j in range(len(n)):
-        print(num[int(n[j])][i], end=' ')
-    print()
+count = 0
+for i in range(1,len(cog)):
+    if (int(cog[i][0]) == 1):
+        count += 1
+print(count)
