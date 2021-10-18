5339c4df1da3cfcaa72936fa2347c6823c816b88
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-18 23:19:58 +0900

    15685 - 드래곤 커브(복습)

--- main.py
+++ main.py
@@ -1,59 +1,24 @@
-n,m = map(int, input().split())
-r,c,d = map(int, input().split())
-matrix = [list(map(int, input().split())) for i in range(n)]
-isclean = [[0 for j in range(m)] for i in range(n)]
-result = 0
+n = int(input())
+matrix = [[0 for i in range(101)] for i in range(101)]
+dy = [0, -1, 0, 1]
+dx = [1, 0, -1, 0]
+ans = 0
+for i in range(n):
+    x, y, d, g = map(int, input().split())
+    matrix[y][x] = 1
+    temp = [d]
+    q = [d]
+    for j in range(g + 1):
+        for k in q:
+            x += dx[k]
+            y += dy[k]
+            matrix[y][x] = 1
+        q = [(i + 1) % 4 for i in temp]
+        q.reverse()
+        temp = temp + q
 
-while(True):
-    if (isclean[r][c] == 0):
-        isclean[r][c] = 1
-        result += 1
-    isTurn = 0
-    for i in range(4):
-        if (d == 0):
-            d = 3
-            if (matrix[r][c-1] != 1 and isclean[r][c-1] == 0):
-                c = c-1
-                isTurn = 1
-                break
-        elif (d == 1):
-            d = 0
-            if (matrix[r-1][c] != 1 and isclean[r-1][c] == 0):
-                r = r-1
-                isTurn = 1
-                break
-        elif (d == 2):
-            d = 1
-            if (matrix[r][c+1] != 1 and isclean[r][c+1] == 0):
-                c = c+1
-                isTurn = 1
-                break
-        elif (d == 3):
-            d = 2
-            if (matrix[r+1][c] != 1 and isclean[r+1][c] == 0):
-                r = r+1
-                isTurn = 1
-                break
-    if (isTurn == 0):
-        if (d == 0):
-            if (matrix[r+1][c] != 1):
-                r = r+1
-            else:
-                break
-        elif (d == 1):
-            if (matrix[r][c-1] != 1):
-                c = c-1
-            else:
-                break
-        elif (d == 2):
-            if (matrix[r-1][c] != 1):
-                r = r-1
-            else:
-                break
-        elif (d == 3):
-            if (matrix[r][c+1] != 1):
-                c = c+1
-            else:
-                break
-
-print(result)
+for i in range(100):
+    for j in range(100):
+        if matrix[i][j] and matrix[i][j + 1] and matrix[i + 1][j] and matrix[i + 1][j + 1]:
+            ans += 1
+print(ans)
