b577f1ecf595ed6b7d9d83c105970369f3a2cbda
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-18 23:49:52 +0900

    11866 - 요세푸스 문제 0

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-n = int(input())
-matrix = [[0 for i in range(101)] for i in range(101)]
-dy = [0, -1, 0, 1]
-dx = [1, 0, -1, 0]
-ans = 0
-for i in range(n):
-    x, y, d, g = map(int, input().split())
-    matrix[y][x] = 1
-    temp = [d]
-    q = [d]
-    for j in range(g + 1):
-        for k in q:
-            x += dx[k]
-            y += dy[k]
-            matrix[y][x] = 1
-        q = [(i + 1) % 4 for i in temp]
-        q.reverse()
-        temp = temp + q
+from collections import deque
+d = deque()
+ans = []
+n,k = map(int, input().split())
+for i in range(1,n+1):
+    d.append(i)
 
-for i in range(100):
-    for j in range(100):
-        if matrix[i][j] and matrix[i][j + 1] and matrix[i + 1][j] and matrix[i + 1][j + 1]:
-            ans += 1
-print(ans)
+while(d):
+    d.rotate(-(k-1))
+    ans.append(d.popleft())
+
+print("<", end="")
+for i in ans:
+    if (i == ans[-1]):
+        print(i, end="")
+    else:
+        print(i, end=", ")
+print(">")
