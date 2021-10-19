e411aff542e64cf241dc0f2acfc6ec7cbf051ed4
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-19 14:02:57 +0900

    2116 - 주사위 뽑기

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-h,w = map(int,input().split())
-matrix = [[0 for i in range(w)] for i in range(h)]
-block = list(map(int,input().split()))
-count = 0
+n = int(input())
+dice = []
+opposite = [5,3,4,1,2,0]
+for i in range(n):
+    dice.append(list(map(int, input().split())))
 
-for i in range(len(block)):
-    n = block[i]
-    for j in range(n):
-        matrix[(h-1)-j][i] = 1
-
-for i in range(h):
-    start = False
-    st = fi = 0
-    for j in range(w):
-        if (matrix[i][j] == 1 and start == False):
-            st = j
-            start = True
-        elif (matrix[i][j] == 1 and start == True):
-            fi = j
-
-    for j in range(st,fi):
-        if (matrix[i][j] == 0):
-            count += 1
-            matrix[i][j] = 2
-
-print(count)
+sum = [0 for i in range(6)]
+for i in range(6):
+    m = 0
+    up = dice[0][opposite[i]]
+    for j in range(6):
+        if (j != i and j != opposite[i]):
+            if (dice[0][j] > m):
+                m = dice[0][j]
+    sum[i] += m
+    for j in range(1, n):
+        s = dice[j].index(up)
+        up = dice[j][opposite[dice[j].index(up)]]
+        m = 0
+        for k in range(6):
+            if (k != s and k != opposite[s]):
+                if (dice[j][k] > m):
+                    m = dice[j][k]
+        sum[i] += m
+print(max(sum))
