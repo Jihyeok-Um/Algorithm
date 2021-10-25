e20d0e1793b934a067cca0268d941fb2c6c75dea
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-26 03:14:47 +0900

    5430 - AC

--- main.py
+++ main.py
@@ -1,18 +1,71 @@
 import sys
-input = sys.stdin.readline
+from collections import deque
+t = int(input())
+d = deque()
+ans = []
+ans2 = deque()
+numList = ['0','1','2','3','4','5','6','7','8','9']
 
-fullStr = list(input().rstrip())
-exStr = list(input().rstrip())
-stack = []
-for i in range(len(fullStr)):
-    stack.append(fullStr[i])
-    if len(stack) >= len(exStr) and stack[-1] == exStr[-1]:
-        if stack[-len(exStr):] == exStr:
-            for j in range(len(exStr)):
-                stack.pop()
+for i in range(t):
+    errer = False
+    func = input()
+    n = int(input())
+    num = sys.stdin.readline().rstrip()
+    ans = ["" for i in range(len(num))]
+    j = 0
+    k = -1
+    r = 0
+    while num[j] != ']':
+        if num[j] in numList:
+            ans[k] += str(num[j])
+            j += 1
+        else:
+            k += 1
+            j += 1
+    ans2.clear()
+    for k in range(len(ans)):
+        if ans[k] != '':
+            ans2.append(int(ans[k]))
 
+    for k in func:
+        if k == 'R':
+            r += 1
+        elif k == 'D':
+            if (len(ans2) == 0):
+                errer = True
+                break
+            else:
+                if r % 2 == 0:
+                    ans2.popleft()
+                if r % 2 == 1:
+                    ans2.pop()
 
-if stack:
-    print("".join(stack))
-else:
-    print("FRULA")
+    if(errer == True):
+        print("error")
+    else:
+        if r % 2 == 1:
+            ans2.reverse()
+            ans3 = list(ans2)
+            if(len(ans3) == 0):
+                print("[]")
+            else:
+                print("[", end="")
+            for i in range(len(ans3)):
+                if(i == len(ans3)-1):
+                    print(ans3[i],end="]")
+                    print()
+                else:
+                    print(ans3[i], end=",")
+        elif r % 2 == 0:
+            ans3 = list(ans2)
+            if (len(ans3) == 0):
+                print("[]")
+            else:
+                print("[", end="")
+            for i in range(len(ans3)):
+                if (i == len(ans3) - 1):
+                    print(ans3[i], end="]")
+                    print()
+                else:
+                    print(ans3[i], end=",")
+
