560e1d6008e39b22cf62e64f3e8fdf0fa5dc3868
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-26 15:18:53 +0900

    12904번 - A와 B

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-from collections import deque
-stack = deque()
-prec = {
-    '*': 3, '/': 3,
-    '+': 2, '-': 2,
-    '(': 1, ')': 1,
-}
-s = input()
-ans = []
-bracket = 0
-for i in range(len(s)):
-    if (s[i] in prec):
-        while (len(stack) != 0 and prec[stack[-1]] >= prec[s[i]]):
-            if (s[i] == '('):
-                break
-            elif(s[i] == ')'):
-                if (stack[-1] != '('):
-                    ans += stack.pop()
-                else:
-                    stack.pop()
-                    break
-            else:
-                ans += stack.pop()
-        if(s[i] != ')'):
-            stack.append(s[i])
-    else:
-        ans += s[i]
-while(len(stack) != 0):
-    if(stack[-1] != '(' and stack[-1] != ')'):
-        ans += stack.pop()
-    else:
-        stack.pop()
 
-for i in ans:
-    print(i,end="")
+
+S = input()
+T = input()
+isGet = False
+
+def getT(T):
+    global isGet
+    if T == S:
+        print(1)
+        isGet = True
+        return
+    if len(T) <= len(S):
+        return
+
+    if T[-1] == 'A':
+        T = T[:-1]
+    else:
+        T = T[:-1]
+        T = T[::-1]
+    getT(T)
+getT(T)
+if isGet == False:
+    print(0)
