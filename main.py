50dda34fc89a4083bdc229ca226e894f71113a04
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-26 16:17:18 +0900

    1013 - Contact

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import re
-string = input()
-p = re.compile('(100+1+|01)+')
-m = p.match(string)
-if (m != None and m[0] == string):
-    print(m[0])
-    print("SUBMARINE")
-else:
-    print(m[0])
-    print("NOISE")
+t = int(input())
+for i in range(t):
+    string = input()
+    p = re.compile('(100+1+|01)+')
+    m = p.fullmatch(string)
+    if (m != None and m[0] == string):
+        print("YES")
+    else:
+        print("NO")
