2dcc38565595e48a74b0f74edd05dc5465e8f7fe
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-26 17:23:10 +0900

    2800 - 괄호제거 (오답고치기)

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-import re
-t = int(input())
-for i in range(t):
-    string = input()
-    p = re.compile('(100+1+|01)+')
-    m = p.fullmatch(string)
-    if (m != None and m[0] == string):
-        print("YES")
-    else:
-        print("NO")
+import sys
+import copy
+string = list(sys.stdin.readline().rstrip())
+bracketNum = 0
+bracketNum2 = 0
+temp = 0
+bracket = [[0,0] for i in range(10)]
+for i in range(len(string)):
+    if string[i] == '(':
+        bracket[bracketNum2][0] = i
+        bracketNum += 1
+        bracketNum2 += 1
+        temp += 1
+    elif string[i] == ')':
+        bracketNum2 -= 1
+        if ()
+        bracket[bracketNum][1] = i
+combi = []
+for i in range(len(bracket)):
+    if (bracket[i] == [0,0]):
+        continue
+    combi.append(bracket[i])
+
+ans = []
+depth = 0
+print(bracket)
+def delBracket(s,depth):
+    global string
+    result = ""
+    if (depth == temp):
+        for i in range(len(s)):
+            if (s[i] != '!'):
+                result += s[i]
+        if (result not in ans):
+            ans.append(result)
+        return
+
+    delBracket(copy.deepcopy(s), depth+1)
+    s[bracket[depth][0]] = "!"
+    s[bracket[depth][1]] = "!"
+    delBracket(copy.deepcopy(s), depth+1)
+
+delBracket(copy.deepcopy(string), 0)
+for i in range(1,len(ans)):
+    print(ans[i])
