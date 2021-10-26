531eba3a41d36b98ddb6507b5a6c0091b8aa9989
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-26 23:00:55 +0900

    2800 - 괄호제거 (오답고치기)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+from collections import deque
 import copy
+stack = []
 string = list(sys.stdin.readline().rstrip())
 bracketNum = 0
 bracketNum2 = 0
@@ -7,23 +9,20 @@ temp = 0
 bracket = [[0,0] for i in range(10)]
 for i in range(len(string)):
     if string[i] == '(':
-        bracket[bracketNum2][0] = i
-        bracketNum += 1
-        bracketNum2 += 1
-        temp += 1
+        stack.append(['(',i])
     elif string[i] == ')':
-        bracketNum2 -= 1
-        if ()
-        bracket[bracketNum][1] = i
+        b1 = stack.pop()
+        bracket[temp] = [b1[1],i]
+        temp += 1
+
 combi = []
 for i in range(len(bracket)):
     if (bracket[i] == [0,0]):
         continue
     combi.append(bracket[i])
-
 ans = []
 depth = 0
-print(bracket)
+combi.sort()
 def delBracket(s,depth):
     global string
     result = ""
@@ -36,8 +35,8 @@ def delBracket(s,depth):
         return
 
     delBracket(copy.deepcopy(s), depth+1)
-    s[bracket[depth][0]] = "!"
-    s[bracket[depth][1]] = "!"
+    s[combi[depth][0]] = "!"
+    s[combi[depth][1]] = "!"
     delBracket(copy.deepcopy(s), depth+1)
 
 delBracket(copy.deepcopy(string), 0)
