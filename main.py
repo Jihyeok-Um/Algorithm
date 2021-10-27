cb5b53e16db6e8b05fc8864e7558f9b8b03ca8e7
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-27 13:47:26 +0900

    17214 - 다항 함수의 적분

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
 import sys
+string = list(sys.stdin.readline().rstrip())
+num = ""
+num2 = ""
+prec = ""
+prec2 = ""
+x = ""
+i = 0
+isTwo = False
+while True:
+    if (len(string) == i):
+        break
+    if string[i] == '-':
+        prec = string[i]
+    else:
+        if string[i] != 'x':
+            num += string[i]
+        else:
+            x = string[i]
+            i += 1
+            break
+    i += 1
+while len(string) != i:
+    isTwo = True
+    if string[i] == '+' or string[i] == '-':
+        prec2 = string[i]
+    else:
+        num2 += string[i]
+    i += 1
 
-def getNextArray(array):
-    i = len(array)-1
-    while (i > 0 and array[i-1] >= array[i]):
-        i -= 1
-    if (i == 0):
-        for i in range(len(array)):
-            print(array[i], end='')
-        print()
-        return
-
-    j = len(array)-1
-    while (array[j] <= array[i-1]):
-        j -= 1
-    array[j], array[i-1] = array[i-1], array[j]
-
-    j = len(array) - 1
-    while (i <= j):
-        array[j], array[i] = array[i], array[j]
-        i += 1
-        j -= 1
-
-    for i in range(len(array)):
-        print(array[i], end='')
-    print()
-
-    return
-
-def main():
-    t = int(input())
-    for i in range(t):
-        array = list(sys.stdin.readline().rstrip())
-        getNextArray(array)
-
-main()
+if (isTwo):
+    num = int(int(num)/2)
+    num2 = int(num2)
+    if (num != 1 and num2 != 1 and num2 != 0):
+        print(prec + str(num) + "xx" + prec2 + str(num2) + "x+W")
+    elif (num2 != 1 and num == 1 and num2 != 0):
+        print(prec + "xx" + prec2 + str(num2) + "x+W")
+    elif (num != 1 and num2 == 1 and num2 != 0):
+        print(prec + str(num) + "xx" + prec2 + "x+W")
+    elif (num == 1 and num2 == 1 and num2 != 0):
+        print(prec + "xx" + prec2 + "x+W")
+    elif (num2 == 0 and num == 1):
+        print(prec + "xx" + "+W")
+    elif (num2 == 0 and num != 1):
+        print(prec + str(num) + "xx" + prec2 + "W")
+else:
+    if (x == ""):
+        num = int(num)
+    else:
+        num = int(int(num)/2)
+    if (num != 1 and num != 0):
+        print(prec + str(num) + x + "x" + "+W")
+    elif (num == 1):
+        print(prec + x + "x" + "+W")
+    elif (num == 0):
+        print("W")
