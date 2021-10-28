3c2517f4f048facbc2723f4e8407e8325b029fe6
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-28 13:47:32 +0900

    15927 - 회문은 회문아니야!

--- main.py
+++ main.py
@@ -1,56 +1,30 @@
-import sys
-string = list(sys.stdin.readline().rstrip())
-num = ""
-num2 = ""
-prec = ""
-prec2 = ""
-x = ""
-i = 0
-isTwo = False
-while True:
-    if (len(string) == i):
-        break
-    if string[i] == '-':
-        prec = string[i]
-    else:
-        if string[i] != 'x':
-            num += string[i]
+def palindrome(length, s):
+    check = s[0]
+    count = 0
+    for i in range(len(s)):
+        if (s[i] == check):
+            count += 1
+    if (count == len(s)):
+        print(-1)
+        return
+    if (len(s) % 2 == 0):
+        str1 = s[len(s)//2:]
+        str2 = s[:len(s)//2]
+        if(str1 == str2[::-1]):
+            print(len(s)-1)
+            return
         else:
-            x = string[i]
-            i += 1
-            break
-    i += 1
-while len(string) != i:
-    isTwo = True
-    if string[i] == '+' or string[i] == '-':
-        prec2 = string[i]
-    else:
-        num2 += string[i]
-    i += 1
+            print(length)
+            return
+    elif (len(s) % 2 == 1):
+        str1 = s[:len(s)//2+1]
+        str2 = s[len(s)//2:]
+        if (str1 == str2[::-1]):
+            print(len(s) - 1)
+            return
+        else:
+            print(length)
+            return
 
-if (isTwo):
-    num = int(int(num)/2)
-    num2 = int(num2)
-    if (num != 1 and num2 != 1 and num2 != 0):
-        print(prec + str(num) + "xx" + prec2 + str(num2) + "x+W")
-    elif (num2 != 1 and num == 1 and num2 != 0):
-        print(prec + "xx" + prec2 + str(num2) + "x+W")
-    elif (num != 1 and num2 == 1 and num2 != 0):
-        print(prec + str(num) + "xx" + prec2 + "x+W")
-    elif (num == 1 and num2 == 1 and num2 != 0):
-        print(prec + "xx" + prec2 + "x+W")
-    elif (num2 == 0 and num == 1):
-        print(prec + "xx" + "+W")
-    elif (num2 == 0 and num != 1):
-        print(prec + str(num) + "xx" + prec2 + "W")
-else:
-    if (x == ""):
-        num = int(num)
-    else:
-        num = int(int(num)/2)
-    if (num != 1 and num != 0):
-        print(prec + str(num) + x + "x" + "+W")
-    elif (num == 1):
-        print(prec + x + "x" + "+W")
-    elif (num == 0):
-        print("W")
+string = input()
+palindrome(len(string),string)
