df96b276b07a19bb7f5bb2df8449c1676b1d77b3
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-10-28 13:56:50 +0900

    1966 - 프린터 큐

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-def palindrome(length, s):
-    check = s[0]
-    count = 0
-    for i in range(len(s)):
-        if (s[i] == check):
-            count += 1
-    if (count == len(s)):
-        print(-1)
-        return
-    if (len(s) % 2 == 0):
-        str1 = s[len(s)//2:]
-        str2 = s[:len(s)//2]
-        if(str1 == str2[::-1]):
-            print(len(s)-1)
-            return
-        else:
-            print(length)
-            return
-    elif (len(s) % 2 == 1):
-        str1 = s[:len(s)//2+1]
-        str2 = s[len(s)//2:]
-        if (str1 == str2[::-1]):
-            print(len(s) - 1)
-            return
-        else:
-            print(length)
-            return
+from collections import deque
 
-string = input()
-palindrome(len(string),string)
+
+def solution(priorities, location):
+    q = deque()
+    for i in priorities:
+        q.append(i)
+
+    count = 0
+    location += 1
+    while (q):
+        temp = q.popleft()
+        if (location > 0):
+            location -= 1
+
+        if (len(q) >= 1 and temp >= max(q)):
+            count += 1
+            if (location == 0):
+                print(count)
+                return
+        elif (len(q) == 0):
+            count += 1
+            if (location == 0):
+                print(count)
+                return
+        else:
+            q.append(temp)
+            if (location == 0):
+                location = len(q)
+
+t = int(input())
+for i in range(t):
+    n,m = map(int, input().split())
+    k = list(map(int, input().split()))
+    solution(k,m)
