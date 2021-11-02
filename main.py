95b5b4e95f5f20b629cbac05cad883b995ff48d9
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-03 00:51:06 +0900

    프로그래머스 - 나머지가 1이 되는 수 찾기

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-from collections import deque
-n,k = map(int,input().split())
-matrix = [-1 for i in range(200001)]
-q = deque()
-ok = 0
-
-q.append(n)
-matrix[n] = 0
-while(q):
-    x = q.popleft()
-    if(x == k):
-        ok += 1
-    if (x-1 >= 0 and (matrix[x-1] == -1 or matrix[x-1]>=matrix[x]+1)):
-        q.append(x-1)
-        matrix[x-1] = matrix[x] + 1
-    if (x+1 < 200000 and (matrix[x+1] == -1 or matrix[x+1]>=matrix[x]+1)):
-        q.append(x+1)
-        matrix[x+1] = matrix[x] + 1
-    if (x*2 < 200000 and (matrix[x*2] == -1 or matrix[x*2]>=matrix[x]+1)):
-        q.append(x*2)
-        matrix[x*2] = matrix[x] + 1
-
-print(matrix[k])
-print(ok)
+def solution(n):
+    answer = 1
+    while(True):
+        if (n % answer == 1):    
+            return answer
+        else:
+            answer += 1
